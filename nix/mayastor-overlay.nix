9aae0ee47832178a6ccc2bc55c9eea2a73ccefee
Jan Kryl <jan.kryl@mayadata.io>
2019-10-30 16:47:16 +0000

    Add enableDebug param to libspdk nix package
    If set to true it adds --enable-debug configure option to libspdk.
    Currently it is not used by anything. In future we might use it to
    build debug flavour of mayastor.

--- nix/mayastor-overlay.nix
+++ nix/mayastor-overlay.nix
@@ -1,10 +1,10 @@
 self: super: {
   libiscsi = super.callPackage ./pkgs/libiscsi {};
   nvme-cli = super.callPackage ./pkgs/nvme-cli {};
-  libspdk = super.callPackage ./pkgs/libspdk {};
-  mayastor = (super.callPackage ./pkgs/mayastor{}).mayastor;
-  mayastorImage = (super.callPackage ./pkgs/mayastor{}).mayastorImage;
-  mayastorCSIImage = (super.callPackage ./pkgs/mayastor{}).mayastorCSIImage;
+  libspdk = super.callPackage ./pkgs/libspdk { enableDebug = false; };
+  mayastor = (super.callPackage ./pkgs/mayastor {}).mayastor;
+  mayastorImage = (super.callPackage ./pkgs/mayastor {}).mayastorImage;
+  mayastorCSIImage = (super.callPackage ./pkgs/mayastor {}).mayastorCSIImage;
   k9s = super.callPackage ./pkgs/k9s {};
   stern = super.callPackage ./pkgs/stern {};
   node-moac = (import ./../csi/moac { pkgs = super; }).package;
