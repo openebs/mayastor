787d36d300aefc9e77be1b2c391615d140ba43cc
Jeffry Molanus <jeffry.molanus@gmail.com>
2019-12-13 17:39:40 +0100

    nexus: add more test cases
    These tests put filesystems ontop of 2-way mirror nexus and
    validate when split, the m5sum of the written data matches.
    
    Additonally, run verify=crc32 FIO workload on the nexus.

--- mayastor/tests/common/mod.rs
+++ mayastor/tests/common/mod.rs
@@ -1,4 +1,5 @@
 use mayastor::mayastor_logger_init;
+use run_script::{self, ScriptOptions};
 use std::{env, io, io::Write, process::Command};
 
 pub fn mayastor_test_init() {
@@ -28,6 +29,17 @@ pub fn truncate_file(path: &str, size: u64) {
         .output()
         .expect("failed exec truncate");
 
+    assert_eq!(output.status.success(), true);
+}
+
+pub fn fscheck(device: &str) {
+    let output = Command::new("fsck")
+        .args(&[device, "-n"])
+        .output()
+        .expect("fsck exec failed");
+
+    io::stdout().write_all(&output.stderr).unwrap();
+    io::stdout().write_all(&output.stdout).unwrap();
     assert_eq!(output.status.success(), true);
 }
 
@@ -66,5 +78,80 @@ pub fn compare_files(a: &str, b: &str) {
         .output()
         .expect("failed to execute \"cmp\"");
 
+    io::stdout().write_all(&output.stderr).unwrap();
+    io::stdout().write_all(&output.stdout).unwrap();
     assert_eq!(output.status.success(), true);
+}
+
+pub fn mount_umount(device: &str) -> String {
+    let (exit, stdout, _stderr) = run_script::run(
+        r#"
+        mkdir /tmp/__test
+        mount $1 /tmp/__test
+        umount /tmp/__test
+        exit 0
+    "#,
+        &vec![device.into()],
+        &run_script::ScriptOptions::new(),
+    )
+    .unwrap();
+    assert_eq!(exit, 0);
+    stdout
+}
+
+pub fn mount_and_write_file(device: &str) -> String {
+    let mut options = ScriptOptions::new();
+    options.capture_output = true;
+    options.exit_on_error = true;
+    options.print_commands = false;
+
+    let (exit, stdout, _stderr) = run_script::run(
+        r#"
+        mkdir /tmp/__test
+        mount $1 /tmp/__test
+        echo test > /tmp/__test/test
+        md5sum /tmp/__test/test
+        umount /tmp/__test
+        rm -rf /tmp/__test
+        exit 0
+    "#,
+        &vec![device.into()],
+        &options,
+    )
+    .unwrap();
+    assert_eq!(exit, 0);
+    stdout.trim_end().to_string()
+}
+
+pub fn mount_and_get_md5(device: &str) -> String {
+    let (exit, stdout, _stderr) = run_script::run(
+        r#"
+        mkdir /tmp/__test
+        mount $1 /tmp/__test
+        md5sum /tmp/__test/test
+        umount /tmp/__test
+        rm -rf /tmp/__test
+        exit 0
+    "#,
+        &vec![device.into()],
+        &run_script::ScriptOptions::new(),
+    )
+    .unwrap();
+    assert_eq!(exit, 0);
+    stdout
+}
+
+pub fn fio_run_verify(device: &str) -> String {
+    let (exit, stdout, _stderr) = run_script::run(
+        r#"
+        fio --name=randrw --rw=randrw --ioengine=libaio --direct=1 --time_based=1 \
+        --runtime=60 --bs=4k --verify=crc32 --group_reporting=1 --output-format=terse \
+        --verify_fatal=1 --verify_async=2 --filename=$1
+    "#,
+        &vec![device.into()],
+        &run_script::ScriptOptions::new(),
+    )
+    .unwrap();
+    assert_eq!(exit, 0);
+    stdout
 }
--- mayastor/tests/mount_fs.rs
+++ mayastor/tests/mount_fs.rs
@@ -1,4 +1,5 @@
 use futures::channel::oneshot;
+use log::*;
 use mayastor::{
     bdev::nexus::{
         nexus_bdev::{nexus_create, nexus_lookup},
@@ -18,9 +19,9 @@ pub mod common;
 #[test]
 fn mount_fs() {
     common::mayastor_test_init();
-    let args = vec!["io_type", "-m", "0x3"];
+    let args = vec!["io_type", "-m", "0x3", "-L", "bdev"];
 
-    common::dd_random_file(DISKNAME1, 4096, 64 * 1024);
+    common::truncate_file(DISKNAME1, 64 * 1024);
     common::truncate_file(DISKNAME2, 64 * 1024);
 
     let rc: i32 = mayastor_start("test", args, || {
@@ -38,7 +39,20 @@ async fn create_nexus() {
         .unwrap();
 }
 
-async fn works() {
+async fn create_nexus_splitted() {
+    let ch = vec![BDEVNAME1.to_string()];
+    nexus_create("left", 64 * 1024 * 1024, None, &ch)
+        .await
+        .unwrap();
+
+    let ch = vec![BDEVNAME2.to_string()];
+    nexus_create("right", 64 * 1024 * 1024, None, &ch)
+        .await
+        .unwrap();
+}
+
+async fn mirror_fs_test<'a>(fstype: String) {
+    info!("running mirror test: {}", fstype);
     create_nexus().await;
     let nexus = nexus_lookup("nexus").unwrap();
 
@@ -51,15 +65,78 @@ async fn works() {
     assert_eq!(false, nexus.io_is_supported(nexus_io::io_type::UNMAP));
 
     let device = nexus.share(None).await.unwrap();
-    let (s, r) = oneshot::channel::<bool>();
+    let (s, r) = oneshot::channel::<String>();
 
-    // we cannot block the reactor
+    // create an XFS filesystem on the nexus device, mount it, create a file and
+    // return the md5 of that file
+
     std::thread::spawn(move || {
-        common::mkfs(&device, "xfs");
-        common::mkfs(&device, "ext4");
-        s.send(true)
+        common::mkfs(&device, &fstype);
+        let md5 = common::mount_and_write_file(&device);
+        s.send(md5)
     });
 
     r.await.unwrap();
+    // destroy the share and the nexus
+    nexus.unshare().await.unwrap();
+    nexus.destroy().await;
+
+    // create a split nexus, i.e two nexus devices which each one leg of the
+    // mirror
+    create_nexus_splitted().await;
+
+    let left = nexus_lookup("left").unwrap();
+    let right = nexus_lookup("right").unwrap();
+
+    // share both nexuses
+    let left_device = left.share(None).await.unwrap();
+    let right_device = right.share(None).await.unwrap();
+
+    let (s, r) = oneshot::channel::<String>();
+
+    // read back the md5 from the left leg
+    //
+    // XXX note -- as the filesystems are mirrors of one and other, you cannot
+    // mount them both at the same time. This will be rejected by XFS
+    // because they have the same exact UUID
+    //
+
+    std::thread::spawn(move || s.send(common::mount_and_get_md5(&left_device)));
+
+    let md5_left = r.await.unwrap();
+    left.unshare().await.unwrap();
+    left.destroy().await;
+
+    // read the md5 of the right side of the mirror
+    let (s, r) = oneshot::channel::<String>();
+    std::thread::spawn(move || {
+        s.send(common::mount_and_get_md5(&right_device))
+    });
+
+    let md5_right = r.await.unwrap();
+
+    right.unshare().await.unwrap();
+    right.destroy().await;
+    assert_eq!(md5_left, md5_right);
+}
+
+async fn run_fio_on_nexus() {
+    create_nexus().await;
+    let nexus = nexus_lookup("nexus").unwrap();
+
+    let device = nexus.share(None).await.unwrap();
+    let (s, r) = oneshot::channel::<String>();
+
+    std::thread::spawn(move || s.send(common::fio_run_verify(&device)));
+
+    r.await.unwrap();
+    nexus.destroy().await;
+}
+
+async fn works() {
+    mirror_fs_test("xfs".into()).await;
+    mirror_fs_test("ext4".into()).await;
+    run_fio_on_nexus().await;
+
     mayastor_stop(0);
 }
