c3dcd0015abf63c51a6772488b7960ecf04f93ef
Jan Kryl <jan.kryl@mayadata.io>
2020-01-07 14:28:39 +0000

    Removing k9s and stern pkgs which are available in official channels now.

--- nix/mayastor-overlay.nix
+++ nix/mayastor-overlay.nix
@@ -6,8 +6,6 @@ self: super: {
   mayastor = (super.callPackage ./pkgs/mayastor {}).mayastor;
   mayastorImage = (super.callPackage ./pkgs/mayastor {}).mayastorImage;
   mayastorCSIImage = (super.callPackage ./pkgs/mayastor {}).mayastorCSIImage;
-  k9s = super.callPackage ./pkgs/k9s {};
-  stern = super.callPackage ./pkgs/stern {};
   node-moac = (import ./../csi/moac { pkgs = super; }).package;
   node-moacImage = (import ./../csi/moac { pkgs = super; }).buildImage;
 }
