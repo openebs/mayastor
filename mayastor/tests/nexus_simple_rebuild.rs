6fe3257fe847f21a8e31c9d9b38bd2f8c1130a8e
Tiago Castro <tiagolobocastro@gmail.com>
2020-03-04 13:47:21 +0000

    Simple rebuild cas 37 36 (#1)
    Adding a simple/noddy rebuild and its test
    
    Rebuild up to 10MiB of the child to avoid timing out the js tests: this is temporary until we move the rebuild trigger to another thread context
    
    Add a test case for the rebuild within the add_child rpc call:
    1 - create/start a nexus with 1 child
    2 - dd from urandom into the nexus
    3 - verify (cmp) that the child has the right data
    5 - verify (cmp) that a new child (to be added) has the wrong data
    4 - add the new child and wait for rebuild
    5 - verify (cmp) that the new child has the right data
    
    CAS-37: On child added fully rebuild from another healthy child on the same context with no frontend IO
    CAS-36: Develop the first test case for the child added rebuild process

---
+++ mayastor/tests/nexus_simple_rebuild.rs
@@ -0,0 +1,112 @@
+use crossbeam::channel::unbounded;
+pub mod common;
+
+use mayastor::{
+    bdev::{nexus_create, nexus_lookup},
+    core::{
+        mayastor_env_stop,
+        MayastorCliArgs,
+        MayastorEnvironment,
+        Reactor,
+    },
+};
+
+static DISKNAME1: &str = "/tmp/disk1.img";
+static BDEVNAME1: &str = "aio:///tmp/disk1.img?blk_size=512";
+
+static DISKNAME2: &str = "/tmp/disk2.img";
+static BDEVNAME2: &str = "aio:///tmp/disk2.img?blk_size=512";
+
+static NEXUS_NAME: &str = "rebuild_test";
+static NEXUS_SIZE: u64 = 10 * 1024 * 1024;  // 10MiB
+
+#[test]
+fn rebuild_test() {
+
+    common::truncate_file(DISKNAME1, NEXUS_SIZE / 1024);
+    common::truncate_file(DISKNAME2, NEXUS_SIZE / 1024);
+
+    test_init!();
+
+    Reactor::block_on(rebuild_test_start());
+
+    common::delete_file(&[DISKNAME1.into(), DISKNAME2.into()]);
+}
+
+async fn rebuild_test_start() {
+    create_nexus().await;
+
+    let nexus = nexus_lookup(NEXUS_NAME).unwrap();
+    let device = nexus.share(None).await.unwrap();
+
+    let nexus_device = device.clone();
+    let (s, r) = unbounded::<String>();
+    std::thread::spawn(move || s.send(dd_urandom(&nexus_device)));
+    reactor_poll!(r);
+
+    let nexus_device = device.clone();
+    let (s, r) = unbounded::<String>();
+    std::thread::spawn(move || s.send(compare_nexus_device(&nexus_device, DISKNAME1, true)));
+    reactor_poll!(r);
+    
+    let nexus_device = device.clone();
+    let (s, r) = unbounded::<String>();
+    std::thread::spawn(move || s.send(compare_nexus_device(&nexus_device, DISKNAME2, false)));
+    reactor_poll!(r);
+
+    // add the second child -> atm it's where we rebuild as well
+    nexus.add_child(BDEVNAME2).await.unwrap();
+
+    let (s, r) = unbounded::<String>();
+    std::thread::spawn(move || s.send(compare_devices(DISKNAME1, DISKNAME2, true)));
+    reactor_poll!(r);
+
+    mayastor_env_stop(0);
+}
+
+async fn create_nexus() {
+    let ch = vec![BDEVNAME1.to_string()];
+    nexus_create(NEXUS_NAME, NEXUS_SIZE, None, &ch)
+        .await
+        .unwrap();
+}
+
+pub fn dd_urandom(device: &str) -> String {
+    let (_, stdout, _stderr) = run_script::run(
+        r#"
+        dd if=/dev/urandom of=$1 conv=fsync,nocreat,notrunc iflag=count_bytes count=`blockdev --getsize64 $1`
+    "#,
+    &vec![device.into()],
+    &run_script::ScriptOptions::new(),
+    )
+    .unwrap();
+    stdout
+}
+
+pub fn compare_nexus_device(nexus_device: &str, device: &str, expected_pass: bool) -> String {
+    let (exit, stdout, _stderr) = run_script::run(
+        r#"
+        cmp -n `blockdev --getsize64 $1` $1 $2 0 5M
+        test $? -eq $3
+    "#,
+    &vec![nexus_device.into(), device.into(), (!expected_pass as i32).to_string()],
+    &run_script::ScriptOptions::new(),
+    )
+    .unwrap();
+    assert_eq!(exit, 0);
+    stdout
+}
+
+pub fn compare_devices(first_device: &str, second_device: &str, expected_pass: bool) -> String {
+    let (exit, stdout, _stderr) = run_script::run(
+        r#"
+        cmp -b $1 $2 5M 5M
+        test $? -eq $3
+    "#,
+    &vec![first_device.into(), second_device.into(), (!expected_pass as i32).to_string()],
+    &run_script::ScriptOptions::new(),
+    )
+    .unwrap();
+    assert_eq!(exit, 0);
+    stdout
+}
