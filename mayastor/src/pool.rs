e0fd59da4b9c3bb9b159368b59457e355e9dde64
Yannis Irvine <yannis.irvine@mayadata.io>
2020-04-01 18:15:37 +0100

    CAS-16: Refactored pool and replica RPC methods to facilitate new gRPC interface for Mayastor.

--- mayastor/src/pool.rs
+++ mayastor/src/pool.rs
@@ -14,6 +14,7 @@ use futures::{
 };
 
 use rpc::jsonrpc as jsondata;
+use snafu::Snafu;
 use spdk_sys::{
     bdev_aio_delete,
     create_aio_bdev,
@@ -35,22 +36,180 @@ use spdk_sys::{
 use crate::{
     core::Bdev,
     ffihelper::{cb_arg, done_cb},
-    jsonrpc::{jsonrpc_register, Code, JsonRpcError, Result},
+    jsonrpc,
+    jsonrpc::RpcErrorCode,
     replica::ReplicaIter,
 };
 
+/// Errors for pool operations.
+#[derive(Debug, Snafu)]
+#[snafu(visibility = "pub(crate)")]
+pub enum Error {
+    #[snafu(display(
+        "Invalid number of disks specified: should be 1, got {}",
+        num
+    ))]
+    BadNumDisks { num: usize },
+    #[snafu(display(
+        "AIO bdev {} already exists or parameters are invalid",
+        name
+    ))]
+    BadBdev { name: String },
+    #[snafu(display("Base bdev {} already exists", name))]
+    AlreadyBdev { name: String },
+    #[snafu(display("Base bdev {} does not exist", name))]
+    UnknownBdev { name: String },
+    #[snafu(display("The pool {} already exists", name))]
+    AlreadyExists { name: String },
+    #[snafu(display("The pool {} does not exist", name))]
+    UnknownPool { name: String },
+    #[snafu(display("Could not create pool {}", name))]
+    BadCreate { name: String },
+    #[snafu(display("Failed to create the pool {} (errno={})", name, errno))]
+    FailedCreate { name: String, errno: i32 },
+    #[snafu(display("The pool {} disappeared", name))]
+    PoolGone { name: String },
+    #[snafu(display("The device {} hosts another pool", name))]
+    DeviceAlreadyUsed { name: String },
+    #[snafu(display("Failed to import the pool {} (errno={})", name, errno))]
+    FailedImport { name: String, errno: i32 },
+    #[snafu(display("Failed to unshare replica: {}", msg))]
+    FailedUnshareReplica { msg: String },
+    #[snafu(display("Failed to destroy pool {} (errno={})", name, errno))]
+    FailedDestroyPool { name: String, errno: i32 },
+    #[snafu(display(
+        "Failed to destroy base bdev {} for the pool {} (errno={})",
+        bdev,
+        name,
+        errno
+    ))]
+    FailedDestroyBdev {
+        bdev: String,
+        name: String,
+        errno: i32,
+    },
+}
+
+impl jsonrpc::RpcErrorCode for Error {
+    fn rpc_error_code(&self) -> jsonrpc::Code {
+        match self {
+            Error::BadNumDisks {
+                ..
+            } => jsonrpc::Code::InvalidParams,
+            Error::BadBdev {
+                ..
+            } => jsonrpc::Code::InvalidParams,
+            Error::AlreadyBdev {
+                ..
+            } => jsonrpc::Code::InvalidParams,
+            Error::UnknownBdev {
+                ..
+            } => jsonrpc::Code::NotFound,
+            Error::AlreadyExists {
+                ..
+            } => jsonrpc::Code::AlreadyExists,
+            Error::UnknownPool {
+                ..
+            } => jsonrpc::Code::NotFound,
+            Error::BadCreate {
+                ..
+            } => jsonrpc::Code::InvalidParams,
+            Error::FailedCreate {
+                ..
+            } => jsonrpc::Code::InvalidParams,
+            Error::PoolGone {
+                ..
+            } => jsonrpc::Code::InternalError,
+            Error::DeviceAlreadyUsed {
+                ..
+            } => jsonrpc::Code::InvalidParams,
+            Error::FailedImport {
+                ..
+            } => jsonrpc::Code::InternalError,
+            Error::FailedUnshareReplica {
+                ..
+            } => jsonrpc::Code::InternalError,
+            Error::FailedDestroyPool {
+                ..
+            } => jsonrpc::Code::InternalError,
+            Error::FailedDestroyBdev {
+                ..
+            } => jsonrpc::Code::InternalError,
+        }
+    }
+}
+
+impl From<Error> for jsonrpc::JsonRpcError {
+    fn from(e: Error) -> Self {
+        Self {
+            code: e.rpc_error_code(),
+            message: e.to_string(),
+        }
+    }
+}
+
+impl From<Error> for tonic::Status {
+    fn from(e: Error) -> Self {
+        match e {
+            Error::BadNumDisks {
+                ..
+            } => Self::invalid_argument(e.to_string()),
+            Error::BadBdev {
+                ..
+            } => Self::invalid_argument(e.to_string()),
+            Error::AlreadyBdev {
+                ..
+            } => Self::invalid_argument(e.to_string()),
+            Error::UnknownBdev {
+                ..
+            } => Self::not_found(e.to_string()),
+            Error::AlreadyExists {
+                ..
+            } => Self::already_exists(e.to_string()),
+            Error::UnknownPool {
+                ..
+            } => Self::not_found(e.to_string()),
+            Error::BadCreate {
+                ..
+            } => Self::invalid_argument(e.to_string()),
+            Error::FailedCreate {
+                ..
+            } => Self::invalid_argument(e.to_string()),
+            Error::PoolGone {
+                ..
+            } => Self::not_found(e.to_string()),
+            Error::DeviceAlreadyUsed {
+                ..
+            } => Self::unavailable(e.to_string()),
+            Error::FailedImport {
+                ..
+            } => Self::internal(e.to_string()),
+            Error::FailedUnshareReplica {
+                ..
+            } => Self::internal(e.to_string()),
+            Error::FailedDestroyPool {
+                ..
+            } => Self::internal(e.to_string()),
+            Error::FailedDestroyBdev {
+                ..
+            } => Self::internal(e.to_string()),
+        }
+    }
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
 /// Wrapper for create aio bdev C function
-fn create_base_bdev(file: &str, block_size: u32) -> Result<()> {
+pub fn create_base_bdev(file: &str, block_size: u32) -> Result<()> {
     debug!("Creating aio bdev {} ...", file);
     let cstr_file = CString::new(file).unwrap();
     let rc = unsafe {
         create_aio_bdev(cstr_file.as_ptr(), cstr_file.as_ptr(), block_size)
     };
     if rc != 0 {
-        Err(JsonRpcError::new(
-            Code::InvalidParams,
-            "AIO bdev already exists or parameters are invalid",
-        ))
+        Err(Error::BadBdev {
+            name: String::from(file),
+        })
     } else {
         info!("aio bdev {} was created", file);
         Ok(())
@@ -73,7 +232,7 @@ extern "C" fn pool_done_cb(
 ///
 /// Note about safety: The structure wraps raw C pointers from SPDK.
 /// It is safe to use only in synchronous context. If you keep Pool for
-/// longer than that then something else can run on reactor_0 inbetween
+/// longer than that then something else can run on reactor_0 between
 /// which may destroy the pool and invalidate the pointers!
 pub struct Pool {
     lvs_ptr: *mut spdk_lvol_store,
@@ -151,10 +310,9 @@ impl Pool {
         let base_bdev = match Bdev::lookup_by_name(disk) {
             Some(bdev) => bdev,
             None => {
-                return Err(JsonRpcError::new(
-                    Code::NotFound,
-                    format!("Base bdev {} does not exist", disk),
-                ))
+                return Err(Error::UnknownBdev {
+                    name: String::from(disk),
+                })
             }
         };
         let pool_name = CString::new(name).unwrap();
@@ -175,21 +333,17 @@ impl Pool {
         };
         // TODO: free sender
         if rc < 0 {
-            return Err(JsonRpcError::new(
-                Code::InvalidParams,
-                format!("Failed to create the pool {}", name),
-            ));
+            return Err(Error::BadCreate {
+                name: String::from(name),
+            });
         }
 
         let lvs_errno = receiver.await.expect("Cancellation is not supported");
         if lvs_errno != 0 {
-            return Err(JsonRpcError::new(
-                Code::InvalidParams,
-                format!(
-                    "Failed to create the pool {} (errno={})",
-                    name, lvs_errno
-                ),
-            ));
+            return Err(Error::FailedCreate {
+                name: String::from(name),
+                errno: lvs_errno,
+            });
         }
 
         match Pool::lookup(&name) {
@@ -197,10 +351,9 @@ impl Pool {
                 info!("The pool {} has been created", name);
                 Ok(pool)
             }
-            None => Err(JsonRpcError::new(
-                Code::InternalError,
-                format!("The pool {} disappeared", name),
-            )),
+            None => Err(Error::PoolGone {
+                name: String::from(name),
+            }),
         }
     }
 
@@ -209,10 +362,9 @@ impl Pool {
         let base_bdev = match Bdev::lookup_by_name(disk) {
             Some(bdev) => bdev,
             None => {
-                return Err(JsonRpcError::new(
-                    Code::InternalError,
-                    format!("Base bdev {} does not exist", disk),
-                ))
+                return Err(Error::UnknownBdev {
+                    name: String::from(disk),
+                })
             }
         };
 
@@ -235,19 +387,15 @@ impl Pool {
                     info!("The pool {} has been imported", name);
                     Ok(pool)
                 }
-                None => Err(JsonRpcError::new(
-                    Code::InvalidParams,
-                    format!("The device {} hosts another pool", disk),
-                )),
+                None => Err(Error::DeviceAlreadyUsed {
+                    name: String::from(disk),
+                }),
             }
         } else {
-            Err(JsonRpcError::new(
-                Code::InternalError,
-                format!(
-                    "Failed to import the pool {} (errno={})",
-                    name, lvs_errno
-                ),
-            ))
+            Err(Error::FailedImport {
+                name: String::from(name),
+                errno: lvs_errno,
+            })
         }
     }
 
@@ -263,7 +411,9 @@ impl Pool {
             if replica.get_pool_name() == name {
                 // XXX temporary
                 replica.unshare().await.map_err(|err| {
-                    JsonRpcError::new(Code::InternalError, err.to_string())
+                    Error::FailedUnshareReplica {
+                        msg: err.to_string(),
+                    }
                 })?;
             }
         }
@@ -275,13 +425,10 @@ impl Pool {
         }
         let lvs_errno = receiver.await.expect("Cancellation is not supported");
         if lvs_errno != 0 {
-            return Err(JsonRpcError::new(
-                Code::InternalError,
-                format!(
-                    "Failed to destroy pool {} (errno={})",
-                    name, lvs_errno
-                ),
-            ));
+            return Err(Error::FailedDestroyPool {
+                name,
+                errno: lvs_errno,
+            });
         }
 
         // we will destroy base bdev now
@@ -303,13 +450,11 @@ impl Pool {
         }
         let bdev_errno = receiver.await.expect("Cancellation is not supported");
         if bdev_errno != 0 {
-            Err(JsonRpcError::new(
-                Code::InternalError,
-                format!(
-                    "Failed to destroy base bdev {} for the pool {} (errno={})",
-                    base_bdev_name, name, bdev_errno
-                ),
-            ))
+            Err(Error::FailedDestroyBdev {
+                bdev: base_bdev_name,
+                name,
+                errno: bdev_errno,
+            })
         } else {
             info!(
                 "The pool {} and base bdev {} have been destroyed",
@@ -355,14 +500,72 @@ impl Iterator for PoolsIter {
     }
 }
 
-fn list_pools() -> Vec<jsondata::Pool> {
+pub(crate) async fn create_pool(
+    args: rpc::mayastor::CreatePoolRequest,
+) -> Result<()> {
+    // TODO: support RAID-0 devices
+    if args.disks.len() != 1 {
+        return Err(Error::BadNumDisks {
+            num: args.disks.len(),
+        });
+    }
+
+    if Pool::lookup(&args.name).is_some() {
+        return Err(Error::AlreadyExists {
+            name: args.name,
+        });
+    }
+
+    // TODO: We would like to check if the disk is in use, but there
+    // is no easy way how to get this info using available api.
+    let disk = &args.disks[0];
+    if Bdev::lookup_by_name(disk).is_some() {
+        return Err(Error::AlreadyBdev {
+            name: disk.clone(),
+        });
+    }
+    // The block size may be missing or explicitly set to zero. In
+    // both cases we want to provide our own default value instead
+    // of SPDK's default which is 512.
+    //
+    // NOTE: Keep this in sync with nexus block size which is
+    // hardcoded to 4096.
+    let mut block_size = args.block_size; //.unwrap_or(0);
+    if block_size == 0 {
+        block_size = 4096;
+    }
+    create_base_bdev(disk, block_size)?;
+
+    if Pool::import(&args.name, disk).await.is_ok() {
+        return Ok(());
+    }
+    Pool::create(&args.name, disk).await?;
+    Ok(())
+}
+
+pub(crate) async fn destroy_pool(
+    args: rpc::mayastor::DestroyPoolRequest,
+) -> Result<()> {
+    let pool = match Pool::lookup(&args.name) {
+        Some(p) => p,
+        None => {
+            return Err(Error::UnknownPool {
+                name: args.name,
+            });
+        }
+    };
+    pool.destroy().await?;
+    Ok(())
+}
+
+pub(crate) fn list_pools() -> Vec<jsondata::Pool> {
     let mut pools = Vec::new();
 
     for pool in PoolsIter::new() {
         pools.push(jsondata::Pool {
             name: pool.get_name().to_owned(),
             disks: vec![pool.get_base_bdev().name()],
-            // TODO: figure out how to detect state of a pool in spdk
+            // TODO: figure out how to detect state of pool
             state: "online".to_owned(),
             capacity: pool.get_capacity(),
             used: pool.get_capacity() - pool.get_free(),
@@ -376,77 +579,22 @@ pub fn register_pool_methods() {
     // Joining create and import together is questionable and we might split
     // the two operations in future. However not until cache config file
     // feature is implemented and requirements become clear.
-    jsonrpc_register(
+    jsonrpc::jsonrpc_register(
         "create_or_import_pool",
-        |args: jsondata::CreateOrImportPoolArgs| {
-            let fut = async move {
-                // TODO: support RAID-0 devices
-                if args.disks.len() != 1 {
-                    return Err(JsonRpcError::new(
-                        Code::InvalidParams,
-                        "Invalid number of disks specified",
-                    ));
-                }
-
-                if Pool::lookup(&args.name).is_some() {
-                    return Err(JsonRpcError::new(
-                        Code::AlreadyExists,
-                        format!("The pool {} already exists", args.name),
-                    ));
-                }
-
-                // TODO: We would like to check if the disk is in use, but there
-                // is no easy way how to get this info using available api.
-                let disk = &args.disks[0];
-                if Bdev::lookup_by_name(disk).is_some() {
-                    return Err(JsonRpcError::new(
-                        Code::InvalidParams,
-                        format!("Base bdev {} already exists", disk),
-                    ));
-                }
-                // The block size may be missing or explicitly set to zero. In
-                // both cases we want to provide our own default value instead
-                // of SPDK's default which is 512.
-                //
-                // NOTE: Keep this in sync with nexus block size which is
-                // hardcoded to 4096.
-                let mut block_size = args.block_size.unwrap_or(0);
-                if block_size == 0 {
-                    block_size = 4096;
-                }
-                if let Err(err) = create_base_bdev(disk, block_size) {
-                    return Err(err);
-                };
-
-                if Pool::import(&args.name, disk).await.is_ok() {
-                    return Ok(());
-                }
-                match Pool::create(&args.name, disk).await {
-                    Ok(_) => Ok(()),
-                    Err(err) => Err(err),
-                }
-            };
-            fut.boxed_local()
+        |args: rpc::mayastor::CreatePoolRequest| {
+            create_pool(args).boxed_local()
         },
     );
 
-    jsonrpc_register("destroy_pool", |args: jsondata::DestroyPoolArgs| {
-        let fut = async move {
-            let pool = match Pool::lookup(&args.name) {
-                Some(p) => p,
-                None => {
-                    return Err(JsonRpcError::new(
-                        Code::NotFound,
-                        format!("The pool {} does not exist", args.name),
-                    ));
-                }
-            };
-            pool.destroy().await
-        };
-        fut.boxed_local()
-    });
+    jsonrpc::jsonrpc_register(
+        "destroy_pool",
+        |args: rpc::mayastor::DestroyPoolRequest| {
+            destroy_pool(args).boxed_local()
+        },
+    );
 
-    jsonrpc_register::<(), _, _, JsonRpcError>("list_pools", |_| {
-        future::ok(list_pools()).boxed_local()
-    });
+    jsonrpc::jsonrpc_register::<(), _, _, jsonrpc::JsonRpcError>(
+        "list_pools",
+        |_| future::ok(list_pools()).boxed_local(),
+    );
 }
