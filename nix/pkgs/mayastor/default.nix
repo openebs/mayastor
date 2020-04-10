9b5c59dc0e727f15f3a0fd35010f8082488df868
Jeffry Molanus <jeffry.molanus@gmail.com>
2020-04-10 19:05:40 +0200

    nix: set debug enable for default output
    All CI is run with debug - this makes it more in line with that.

--- nix/pkgs/mayastor/default.nix
+++ nix/pkgs/mayastor/default.nix
@@ -29,72 +29,95 @@ let
     cargo = channel.stable.cargo;
   };
 in
-rec {
+  with pkgs; rec {
 
-  mayastor = rustPlatform.buildRustPackage rec {
-    name = "mayastor";
-    cargoSha256 = "1q7gnq4zxw0jj6za7f9a25z0mwghfpixqs1dsx0i87bxja2vvmb0";
-    version = "unstable";
-    src = ../../../.;
+    whitelistSource = src: allowedPrefixes:
+      builtins.filterSource
+        (path: type:
+          pkgs.lib.any
+            (allowedPrefix:
+              pkgs.lib.hasPrefix (toString (src + "/${allowedPrefix}")) path)
+            allowedPrefixes) src;
 
-    LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib";
+    mayastor = rustPlatform.buildRustPackage rec {
+      name = "mayastor";
+      cargoSha256 = "1q7gnq4zxw0jj6za7f9a25z0mwghfpixqs1dsx0i87bxja2vvmb0";
+      version = "unstable";
+      src = whitelistSource ../../../. [
+        "Cargo.lock"
+        "Cargo.toml"
+        "csi"
+        "cli"
+        "jsonrpc"
+        "mayastor"
+        "rpc"
+        "spdk-sys"
+        "sysfs"
+        "nvmeadm"
+        ".git"
+      ];
 
-    # these are required for building the proto files that tonic can't find otherwise.
-    PROTOC = "${pkgs.protobuf}/bin/protoc";
-    PROTOC_INCLUDE = "${pkgs.protobuf}/include";
-    C_INCLUDE_PATH = "${libspdk}/include/spdk";
+      LIBCLANG_PATH = "${pkgs.llvmPackages.libclang}/lib";
 
-    buildInputs = [
-      clang
-      e2fsprogs
-      libaio
-      libiscsi.lib
-      libspdk
-      liburing
-      llvmPackages.libclang
-      numactl
-      openssl
-      pkg-config
-      protobuf
-      utillinux
-      xfsprogs
-      utillinux.dev
-    ];
+      # these are required for building the proto files that tonic can't find otherwise.
+      PROTOC = "${pkgs.protobuf}/bin/protoc";
+      PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+      C_INCLUDE_PATH = "${libspdk}/include/spdk";
 
-    doCheck = false;
-    meta = { platforms = stdenv.lib.platforms.linux; };
-  };
+      buildInputs = [
+        clang
+        e2fsprogs
+        libaio
+        libiscsi.lib
+        libspdk
+        liburing
+        llvmPackages.libclang
+        numactl
+        openssl
+        pkg-config
+        protobuf
+        utillinux
+        xfsprogs
+        utillinux.dev
+      ];
 
-  env = pkgs.stdenv.lib.makeBinPath [ pkgs.busybox pkgs.utillinux ];
-  # mkfs.* and mount commands need to be in the PATH for mayastor-agent to work.
-  # For NIX these run-time dependencies are hidden so it ignores them.
-  # The solution is to create shell script wrapper setting the PATH and calling
-  # mayastor-agent, which makes it apparent for the NIX.
-  mayastorAgent = writeScriptBin "mayastor-agent" ''
-    #!${pkgs.stdenv.shell}
-    PATH=${pkgs.e2fsprogs}/bin:${pkgs.utillinux}/bin:${pkgs.xfsprogs}/bin:$PATH ${mayastor}/bin/mayastor-agent "$@"
-  '';
+      buildType = "debug";
 
-  mayastorImage = pkgs.dockerTools.buildLayeredImage {
-    name = "mayadata/mayastor";
-    tag = "latest";
-    created = "now";
-    contents = [ pkgs.busybox mayastor ];
-    config = {
-      Env = [ "PATH=${env}" ];
-      Entrypoint = [ "/bin/mayastor" ];
+      doCheck = false;
+      meta = { platforms = stdenv.lib.platforms.linux; };
     };
-  };
 
-  mayastorCSIImage = pkgs.dockerTools.buildLayeredImage {
-    name = "mayadata/mayastor-grpc";
-    tag = "latest";
-    created = "now";
-    contents = [ pkgs.busybox mayastorAgent ];
-    config = {
-      Entrypoint = [ "/bin/mayastor-agent" ];
-      ExposedPorts = { "10124/tcp" = { }; };
-      Env = [ "PATH=${env}" ];
+    env = pkgs.stdenv.lib.makeBinPath [ pkgs.busybox pkgs.utillinux ];
+    # mkfs.* and mount commands need to be in the PATH for mayastor-agent to work.
+    # For NIX these run-time dependencies are hidden so it ignores them.
+    # The solution is to create shell script wrapper setting the PATH and calling
+    # mayastor-agent, which makes it apparent for the NIX.
+
+    mayastorAgent = writeScriptBin "mayastor-agent" ''
+      #!${pkgs.stdenv.shell}
+      PATH=${pkgs.e2fsprogs}/bin:${pkgs.utillinux}/bin:${pkgs.xfsprogs}/bin:$PATH ${mayastor}/bin/mayastor-agent "$@"
+    '';
+
+    mayastorImage = pkgs.dockerTools.buildLayeredImage {
+      name = "mayadata/mayastor";
+      tag = "latest";
+      created = "now";
+      contents = [ pkgs.busybox mayastor ];
+      config = {
+        Env = [ "PATH=${env}" ];
+        Entrypoint = [ "/bin/mayastor" ];
+      };
     };
-  };
-}
+
+    mayastorCSIImage = pkgs.dockerTools.buildLayeredImage {
+      name = "mayadata/mayastor-grpc";
+      tag = "latest";
+      created = "now";
+      contents = [ pkgs.busybox mayastorAgent ];
+      config = {
+        Entrypoint = [ "/bin/mayastor-agent" ];
+        ExposedPorts = { "10124/tcp" = { }; };
+        Env = [ "PATH=${env}" ];
+      };
+    };
+  }
