029f1697c72035f1e518534573049573078713b6
Tiago Castro <tiagolobocastro@gmail.com>
2020-04-24 12:54:01 +0100

    rebuild: split rebuild module
    splits the rebuild module into impl and api
    no functional changes
    this should help contain the next set of coming changes
    todo: completely hide the rebuild job and expose the trait only

--- mayastor/src/replicas/mod.rs
+++ mayastor/src/replicas/mod.rs
@@ -1,3 +1,10 @@
-/// Rebuild module
-pub mod rebuild;
 pub mod replica;
+
+pub mod rebuild {
+    /// Rebuild api module
+    pub mod rebuild_api;
+    /// Rebuild implementation module
+    pub mod rebuild_impl;
+
+    pub use rebuild_api::*;
+}
