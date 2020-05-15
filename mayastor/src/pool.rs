openebs/mayastor

commit bcb1cc26578f2b427676be9154edac5f586fdbff

pool: Return base bdev type in list pools
Return a URI in the disks field where the URI scheme contains the
base bdev type.

Add a unit test to test creation of pools that use uring bdevs,
though skip the test if the kernel lacks support, which requires
changes to uring-support to only test for kernel support if the
pathname argument is not present.

mayastor/src/pool.rs
@@ -644,7 +644,11 @@ pub(crate) fn list_pools() -> Vec<jsondata::Pool> {
     for pool in PoolsIter::new() {
         pools.push(jsondata::Pool {
             name: pool.get_name().to_owned(),
-            disks: vec![pool.get_base_bdev().name()],
+            disks: vec![
+                pool.get_base_bdev().driver()
+                    + "://"
+                    + &pool.get_base_bdev().name(),
+            ],
             // TODO: figure out how to detect state of pool
             state: "online".to_owned(),
             capacity: pool.get_capacity(),

mayastor-test/test_replica.js
@@ -34,25 +34,25 @@ var remote; // true if the test suite is run against a remote grpc server
 var implicitDisk;
 
 // Create fake disk device used for testing (size 100M)
-function createTestDisk(done) {
-  exec('truncate -s 100m ' + DISK_FILE, (err, stdout, stderr) => {
+function createTestDisk(diskFile, done) {
+  exec('truncate -s 100m ' + diskFile, (err, stdout, stderr) => {
     if (err) return done(stderr);
 
     common.execAsRoot(
       'losetup',
       // Explicitly set blksiz to 512 to be different from the
       // default in mayastor (4096) to test it.
-      ['--show', '-b', '512', '-f', DISK_FILE],
+      ['--show', '-b', '512', '-f', diskFile],
       (err, stdout) => done(err, stdout ? stdout.trim() : '')
     );
   });
 }
 
 // Destroy the fake disk used for testing (disregard any error).
-function destroyTestDisk(done) {
-  if (implicitDisk != null) {
-    common.execAsRoot('losetup', ['-d', implicitDisk], (err) => {
-      fs.unlink(DISK_FILE, (err) => done());
+function destroyTestDisk(diskFile, loopDev, done) {
+  if (loopDev != null) {
+    common.execAsRoot('losetup', ['-d', loopDev], (err) => {
+      fs.unlink(diskFile, (err) => done());
     });
   } else {
     done();
@@ -124,7 +124,7 @@ describe('replica', function() {
       [
         (next) => {
           if (!disks) {
-            createTestDisk((err, newDisk) => {
+            createTestDisk(DISK_FILE, (err, newDisk) => {
               if (err) return next(err);
 
               implicitDisk = newDisk;
@@ -160,7 +160,7 @@ describe('replica', function() {
           if (!implicitDisk) {
             next();
           } else {
-            destroyTestDisk(next);
+            destroyTestDisk(DISK_FILE, implicitDisk, next);
           }
         },
         common.restoreNbdPerms,
@@ -232,7 +232,10 @@ describe('replica', function() {
       }
       assert.equal(res.used, 0);
       assert.equal(res.state, 'POOL_ONLINE');
-      assert.deepEqual(res.disks, disks);
+      assert.equal(res.disks.length, disks.length);
+      for (let i = 0; i < res.disks.length; ++i) {
+        assert.equal(res.disks[i], 'aio://' + disks[i]);
+      }
       done();
     });
   });
@@ -524,6 +527,65 @@ describe('replica', function() {
       res = res.pools.filter((ent) => ent.name == POOL);
       assert.lengthOf(res, 0);
       done();
+    });
+  });
+
+  describe('uring', function() {
+    before(function(done) {
+      // Skip uring bdev if kernel lacks support
+      const URING_SUPPORT_CMD = path.join(
+        __dirname,
+        '..',
+        'target',
+        'debug',
+        'uring-support'
+      );
+      var self = this;
+      exec(URING_SUPPORT_CMD, (error) => {
+        if (error) {
+          self.skip();
+        }
+        done();
+      });
+    });
+
+    it('should create a pool with uring io_if', (done) => {
+      client.createPool(
+        { name: POOL, disks: disks, io_if: enums.POOL_IO_URING },
+        (err, res) => {
+          if (err) return done(err);
+          assert.lengthOf(Object.keys(res), 0);
+          done();
+        }
+      );
+    });
+
+    it('should list the pool', (done) => {
+      client.listPools({}, (err, res) => {
+        if (err) return done(err);
+
+        res = res.pools.filter((ent) => ent.name == POOL);
+        assert.lengthOf(res, 1);
+        res = res[0];
+
+        assert.equal(res.name, POOL);
+        assert.equal(Math.floor(res.capacity / (1024 * 1024)), 96);
+        assert.equal(res.used, 0);
+        assert.equal(res.state, 'POOL_ONLINE');
+        assert.equal(res.disks.length, disks.length);
+        for (let i = 0; i < res.disks.length; ++i) {
+          assert.equal(res.disks[i], 'uring://' + disks[i]);
+        }
+        done();
+      });
+    });
+
+    it('should destroy the pool', (done) => {
+      client.destroyPool({ name: POOL }, (err, res) => {
+        if (err) return done(err);
+        assert.lengthOf(Object.keys(res), 0);
+        done();
+      });
     });
   });
 

mayastor/src/bdev/uring_util.rs
@@ -1,7 +1,8 @@
 use std::{
+    fs,
     fs::{File, OpenOptions},
     io::{BufRead, BufReader, ErrorKind},
-    os::unix::fs::OpenOptionsExt,
+    os::unix::fs::{FileTypeExt, OpenOptionsExt},
 };
 
 pub fn fs_supports_direct_io(path: &str) -> bool {
@@ -59,6 +60,16 @@ fn get_mount_filesystem(path: &str) -> Option<String> {
 }
 
 pub fn fs_type_supported(path: &str) -> bool {
+    let metadata = match fs::metadata(path) {
+        Ok(m) => m,
+        Err(e) => {
+            eprintln!("metadata: {}", e);
+            return false;
+        }
+    };
+    if metadata.file_type().is_block_device() {
+        return true;
+    }
     match get_mount_filesystem(path) {
         None => {
             println!("Skipping uring bdev, unknown fs");

mayastor/src/bin/uring-support.rs
@@ -8,19 +8,16 @@ fn main() {
         .version("0.1.0")
         .author("Jonathan Teh <jonathan.teh@mayadata.io>")
         .about("Determines io_uring support")
-        .arg(
-            Arg::with_name("uring-path")
-                .required(true)
-                .help("Path to file")
-                .index(1),
-        )
+        .arg(Arg::with_name("uring-path").help("Path to file").index(1))
         .get_matches();
 
-    let path = matches.value_of("uring-path").unwrap();
-
-    let supported = uring_util::fs_supports_direct_io(path)
-        && uring_util::fs_type_supported(path)
-        && uring_util::kernel_support();
+    let supported = match matches.value_of("uring-path") {
+        None => true,
+        Some(path) => {
+            uring_util::fs_supports_direct_io(path)
+                && uring_util::fs_type_supported(path)
+        }
+    } && uring_util::kernel_support();
 
     if supported {
         std::process::exit(0);

commit b0de419caf722cf3afa583ae978bb525748fcfeb

pool: Fix failure to destroy pool
The code that determines the base bdev type inadvertently used
lvs_bdev_ptr after it had already been destroyed. Switch to base_bdev
instead so that that the base bdev type can be determined.

Fixes: bcb1cc2 ("pool: Return base bdev type in list pools")

mayastor/src/pool.rs
@@ -498,7 +498,7 @@ impl Pool {
                 return Ok(());
             }
         };
-        let base_bdev_type = self.get_base_bdev().driver();
+        let base_bdev_type = base_bdev.driver();
         debug!("Destroying bdev type {}", base_bdev_type);
 
         let (sender, receiver) = oneshot::channel::<i32>();