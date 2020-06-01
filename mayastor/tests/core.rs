openebs/mayastor

commit 63bd0788696055a7f084fd708bc3897d894c89a9

nexus: add more test cases for adding children
There was some confusion around adding devices to an existing nexus.
This was mostly caused by the method names themselves where not that
clear about what they do.

online/offline puts an existing child into the online state taking part
in the nexus IO path. (For online this not the true yet because of WIP
around rebuild). The child is not implicitly removed.

registering children -- is done during creating of the nexus, these
methods where initially named add_children but now are called
register_xx.

add/remove will add or remove a child to/from the nexus. And will
attempt to destroy the bdev itself.

mayastor/src/bdev/nexus/nexus_bdev_children.rs
@@ -1,3 +1,27 @@
+//!
+//! This file implements operations to the child bdevs from the context of its
+//! parent.
+//!
+//! `register_children` and `register_child` are should only be used when
+//! building up a new nexus
+//!
+//! `offline_child` and `online_child` should be used to include the child into
+//! the IO path of the nexus currently, online of a child will default the nexus
+//! into the degraded mode as it (may) require a rebuild. This will be changed
+//! in the near future -- online child will not determine if it SHOULD online
+//! but simply does what its told. Therefore, the callee must be careful when
+//! using this method.
+//!
+//! 'fault_child` will do the same as `offline_child` except, it will not close
+//! the child.
+//!
+//! `add_child` will construct a new `NexusChild` and add the bdev given by the
+//! uri to the nexus. The nexus will transition to degraded mode as the new
+//! child requires rebuild first.
+//!
+//! When reconfiguring the nexus, we traverse all our children, create new IO
+//! channels for all children that are in the open state.
+
 use futures::future::join_all;
 
 use crate::bdev::{
@@ -13,12 +37,10 @@ use crate::bdev::{
 };
 
 impl Nexus {
-    /// Add the child bdevs to the nexus instance in the "init state"
-    /// this function should be used when bdevs are added asynchronously
-    /// like for example, when parsing the init file. The examine callback
-    /// will iterate through the list and invoke nexus::online once completed
-
-    pub fn add_children(&mut self, dev_name: &[String]) {
+    /// register children with the nexus, only allowed during the nexus init
+    /// phase
+    pub fn register_children(&mut self, dev_name: &[String]) {
+        assert_eq!(self.state, NexusState::Init);
         self.child_count = dev_name.len() as u32;
         dev_name
             .iter()
@@ -33,8 +55,10 @@ impl Nexus {
             .for_each(drop);
     }
 
-    /// create a bdev based on its URL and add it to the nexus
+    /// register a single child the nexus, only allowed during the nexus init
+    /// phase
     pub async fn register_child(&mut self, uri: &str) -> Result<String, Error> {
+        assert_eq!(self.state, NexusState::Init);
         let name = bdev_create(&uri).await?;
         self.children.push(NexusChild::new(
             uri.to_string(),
@@ -47,7 +71,13 @@ impl Nexus {
         Ok(name)
     }
 
-    pub async fn add_child(&mut self, uri: &str) -> Result<String, Error> {
+    /// add a new child to an existing nexus. note that the child is added and
+    /// opened but not taking part of any new IO's that are submitted to the
+    /// nexus.
+    ///
+    /// The child may require a rebuild first, so the nexus will
+    /// transition to degraded mode when the addition has been successful.
+    pub async fn add_child(&mut self, uri: &str) -> Result<NexusState, Error> {
         let name = bdev_create(&uri).await?;
 
         if let Some(child) = bdev_lookup_by_name(&name) {
@@ -55,7 +85,7 @@ impl Nexus {
                 || self.min_num_blocks() < child.num_blocks()
             {
                 error!(
-                    ":{} child {} has invalid geometry",
+                    "{}: child {} has invalid geometry",
                     self.name,
                     child.name()
                 );
@@ -67,7 +97,7 @@ impl Nexus {
 
         let child = bdev_lookup_by_name(&name);
         if child.is_none() {
-            error!(":{} child should be there but its not!", self.name);
+            error!("{}: child should be there but its not!", self.name);
             return Err(Error::Internal("child does not exist".into()));
         };
 
@@ -75,30 +105,52 @@ impl Nexus {
 
         match child.open(self.size) {
             Ok(name) => {
-                info!(":{} child opened successfully {}", self.name, name);
+                // we have created the bdev, and created a nexusChild struct. To
+                // make use of the device itself the
+                // data and metadata must be validated. The child
+                // will be added and marked as faulted, once the rebuild has
+                // completed the device can transition to online
+
+                info!("{}: child opened successfully {}", self.name, name);
+
+                // mark faulted so that it can never take part in the IO path of
+                // the nexus until brought online.
+
+                child.state = ChildState::Faulted;
                 self.children.push(child);
                 self.child_count += 1;
-                self.sync_labels().await?;
+                // TODO -- rsync labels
+                Ok(self.set_state(NexusState::Degraded))
             }
-            Err(_) => {
+            Err(e) => {
                 error!("{}: failed to open child ", self.name);
                 bdev_destroy(&uri).await?;
+                Err(e)
             }
         }
-
-        Ok(uri.into())
     }
 
     /// Destroy child with given uri.
     /// If the child does not exist the method returns success.
-    pub async fn destroy_child(&mut self, uri: &str) -> Result<(), Error> {
+    pub async fn remove_child(&mut self, uri: &str) -> Result<(), Error> {
         let idx = match self.children.iter().position(|c| c.name == uri) {
             None => return Ok(()),
             Some(val) => val,
         };
+
+        self.children[idx].close()?;
+
+        if self.children[idx].state != ChildState::Closed {
+            error!(
+                ":{} cannot destroy child {:?} close it first {:?}",
+                self.name, self.children[idx].state, self.state
+            );
+            return Err(Error::Invalid("must close me".into()));
+        }
+
         let mut child = self.children.remove(idx);
-        child.destroy().await?;
         self.child_count -= 1;
+        child.destroy().await?;
         Ok(())
     }
 
@@ -118,7 +170,9 @@ impl Nexus {
         Ok(self.set_state(NexusState::Degraded))
     }
 
-    /// online a child and reconfigure the IO channels
+    /// online a child and reconfigure the IO channels. The child is already
+    /// registered, but simpy not opened. This can be required in case where
+    /// a child is misbehaving.
     pub async fn online_child(
         &mut self,
         name: &str,
@@ -126,12 +180,15 @@ impl Nexus {
         trace!("{} Online child request", self.name());
 
         if let Some(child) = self.children.iter_mut().find(|c| c.name == name) {
-            child.open(self.size)?;
-            self.reconfigure(DREvent::ChildOnline).await;
-            if self.is_healthy() {
-                Ok(self.set_state(NexusState::Online))
+            if child.state != ChildState::Closed {
+                Err(Error::Invalid(
+                    "Child is not closed so can not open".into(),
+                ))
             } else {
-                Ok(NexusState::Degraded)
+                child.open(self.size)?;
+                self.reconfigure(DREvent::ChildOnline).await;
+                //TODO should be rebuilding
+                Ok(self.set_state(NexusState::Degraded))
             }
         } else {
             Err(Error::NotFound)

mayastor/src/bdev/nexus/mod.rs
@@ -9,7 +9,7 @@ use crate::{
     nexus_uri::{self, UriError},
 };
 
-#[derive(Debug)]
+#[derive(Debug, PartialOrd, PartialEq)]
 pub enum Error {
     /// Nobody knows
     Internal(String),

mayastor/src/bdev/nexus/nexus_bdev.rs
@@ -214,7 +214,7 @@ impl Nexus {
         });
 
         if let Some(child_bdevs) = child_bdevs {
-            n.add_children(child_bdevs);
+            n.register_children(child_bdevs);
         }
 
         // store a reference to the Self in the bdev structure.
@@ -242,7 +242,19 @@ impl Nexus {
         state
     }
 
-    pub(crate) fn is_healthy(&self) -> bool {
+    /// return the current state of the nexus
+    pub fn status(&self) -> NexusState {
+        self.state
+    }
+
+    /// online is the state where the Nexus has all children in the healthy
+    /// state and is not rebuilding (for example)
+    pub fn is_online(&self) -> bool {
+        self.is_healthy() && self.state == NexusState::Online
+    }
+
+    /// returns true if all the children are open
+    pub fn is_healthy(&self) -> bool {
         !self.children.iter().any(|c| c.state != ChildState::Open)
     }
 
@@ -376,9 +388,7 @@ impl Nexus {
         // however, we cannot change the function in async there, so we
         // do it here.
         for child in self.children.iter_mut() {
-            if child.state == ChildState::Open {
-                let _ = child.close();
-            }
+            let _ = child.close();
             info!("Destroying child bdev {}", child.name);
 
             let r = child.destroy().await;
@@ -672,14 +682,12 @@ pub async fn nexus_create(
         }
     }
 
-    let opened = ni.open().await;
-
-    if opened.is_ok() {
-        nexus_list.push(ni);
-        Ok(())
-    } else {
-        ni.destroy_children().await;
-        Err(Error::Internal("Failed to open the nexus".to_owned()))
+    match ni.open().await {
+        Ok(_) => {
+            nexus_list.push(ni);
+            Ok(())
+        }
+        Err(e) => Err(e),
     }
 }
 

mayastor/src/bdev/nexus/nexus_child.rs
@@ -27,15 +27,15 @@ use std::rc::Rc;
 
 #[derive(Debug, Clone, Copy, Serialize, PartialEq)]
 pub(crate) enum ChildState {
-    /// child has not been opened but we are in the process of opening it
+    /// child has not been opened, but we are in the process of opening it
     Init,
-    /// can not add this bdev to the parent as its incompatible property wise
+    /// cannot add this bdev to the parent as its incompatible property wise
     ConfigInvalid,
     /// the child is open for RW
     Open,
     /// The child has been closed by its parent
     Closed,
-    /// a fatal error (or errors) have occurred on this child
+    /// a non-fatal have occurred on this child
     Faulted,
 }
 
@@ -93,14 +93,22 @@ impl Display for NexusChild {
 }
 
 impl NexusChild {
-    /// open the child bdev in RW mode. The bdev can only be opened once in RW
-    /// mode even by the same module. For multiple writers, use we use the
-    /// Descriptor.
+    /// Open the child in RW mode and claim the device to be ours. If the child
+    /// is already opened by someone else (i.e one of the targets) it will
+    /// error out.
+    ///
+    /// only devices in the closed or Init state can be opened.
     pub(crate) fn open(
         &mut self,
         parent_size: u64,
     ) -> Result<String, nexus::Error> {
         trace!("{}: Opening child device {}", self.parent, self.name);
+
+        if self.state != ChildState::Closed && self.state != ChildState::Init {
+            return Err(Error::Invalid(
+                "child is not closed can only open closed".into(),
+            ));
+        }
 
         if let Some(bdev) = self.bdev.as_ref() {
             let child_size = bdev.size_in_bytes();
@@ -176,20 +184,31 @@ impl NexusChild {
     /// close the bdev -- we have no means of determining if this succeeds
     pub(crate) fn close(&mut self) -> Result<ChildState, nexus::Error> {
         trace!("{}: Closing child {}", self.parent, self.name);
+
+        debug!(
+            ":{} has {} references to the descriptor",
+            self.parent,
+            Rc::strong_count(self.descriptor.as_ref().unwrap())
+        );
+
         if let Some(bdev) = self.bdev.as_ref() {
             unsafe {
                 spdk_bdev_module_release_bdev(bdev.inner);
             }
         }
 
-        let _ = self.descriptor.take();
-        // we leave the bdev structure around for when we want reopen it
+        // just to be explicit
+        let descriptor = self.descriptor.take();
+        drop(descriptor);
+
+        // we leave the child structure around for when we want reopen it
         self.state = ChildState::Closed;
         Ok(self.state)
     }
 
+    /// called to get a channel to this child
     pub(crate) fn get_io_channel(&self) -> *mut spdk_sys::spdk_io_channel {
-        assert_eq!(self.state, ChildState::Open);
+        assert_ne!(self.state, ChildState::Closed);
         unsafe { spdk_bdev_get_io_channel(self.desc) }
     }
 
@@ -208,9 +227,7 @@ impl NexusChild {
 
     /// destroy the child bdev
     pub(crate) async fn destroy(&mut self) -> Result<(), nexus::Error> {
-        if self.state == ChildState::Open {
-            self.close().unwrap();
-        }
+        assert_eq!(self.state, ChildState::Closed);
 
         if let Ok(child_type) = nexus_parse_uri(&self.name) {
             match child_type {
@@ -220,7 +237,7 @@ impl NexusChild {
                 BdevType::Bdev(_name) => Ok(()),
             }
         } else {
-            // a bdev type we dont support is being used by the nexus
+            // a bdev type we don't support is being used by the nexus
             Err(nexus::Error::Invalid(format!(
                 "requested bdev: {} type is not supported by the nexus",
                 self.name
@@ -228,8 +245,9 @@ impl NexusChild {
         }
     }
 
+    /// returns if a child can be written too
     pub fn can_rw(&self) -> bool {
-        self.state == ChildState::Open
+        self.state == ChildState::Open || self.state == ChildState::Faulted
     }
 
     pub async fn probe_label(&mut self) -> Result<NexusLabel, nexus::Error> {
@@ -266,7 +284,10 @@ impl NexusChild {
         let mut label = GPTHeader::from_slice(buf.as_slice());
 
         if label.is_err() {
-            info!("{}: {}: Primary label is invalid!", self.parent, self.name);
+            info!(
+                "{}: {}: The primary label is invalid!",
+                self.parent, self.name
+            );
             self.read_at(secondary, &mut buf).await?;
             label = GPTHeader::from_slice(buf.as_slice());
         }

mayastor/src/bdev/nexus/nexus_rebuild.rs
@@ -9,8 +9,8 @@ use crate::{
         Error,
     },
     descriptor::Descriptor,
-    event::spawm_on_core,
-    rebuild::RebuildTask,
+    event::spawn_on_core,
+    rebuild::{RebuildState, RebuildTask},
 };
 use std::rc::Rc;
 
@@ -73,7 +73,7 @@ impl Nexus {
         let copy_task =
             RebuildTask::new(source.unwrap(), target.unwrap()).unwrap();
 
-        let ctx = spawm_on_core(core, copy_task, |task| task.run());
+        let ctx = spawn_on_core(core, copy_task, |task| task.run());
 
         if let Ok(ctx) = ctx {
             self.rebuild_handle = Some(ctx);
@@ -83,14 +83,16 @@ impl Nexus {
         }
     }
 
-    pub async fn rebuild_completion(&mut self) -> Result<bool, Error> {
+    pub async fn rebuild_completion(&mut self) -> Result<RebuildState, Error> {
         if let Some(task) = self.rebuild_handle.as_mut() {
-            if let Ok(r) = task.completed().await {
-                let _ = self.rebuild_handle.take();
-                Ok(r)
-            } else {
-                Ok(false)
-            }
+            let result = match task.completed().await {
+                Ok(state) => Ok(state),
+                Err(_) => Err(Error::Internal(
+                    "rebuild failed; sender is gone".into(),
+                )),
+            };
+            let _ = self.rebuild_handle.take();
+            result
         } else {
             Err(Error::Invalid("No rebuild task registered".into()))
         }

mayastor/src/bdev/nexus/nexus_rpc.rs
@@ -213,7 +213,7 @@ pub(crate) fn register_rpc_methods() {
     jsonrpc_register("add_child_nexus", |args: AddChildNexusRequest| {
         let fut = async move {
             let nexus = nexus_lookup(&args.uuid)?;
-            match nexus.register_child(&args.uri).await {
+            match nexus.add_child(&args.uri).await {
                 Ok(_) => Ok(()),
                 Err(err) => Err(JsonRpcError::new(
                     Code::InternalError,
@@ -227,7 +227,7 @@ pub(crate) fn register_rpc_methods() {
     jsonrpc_register("remove_child_nexus", |args: RemoveChildNexusRequest| {
         let fut = async move {
             let nexus = nexus_lookup(&args.uuid)?;
-            match nexus.destroy_child(&args.uri).await {
+            match nexus.remove_child(&args.uri).await {
                 Ok(_) => Ok(()),
                 Err(err) => Err(JsonRpcError::new(
                     Code::InternalError,

mayastor/src/event.rs
@@ -41,7 +41,7 @@ impl Drop for Mthread {
 ///
 /// Async closures are not supported (yet) as there is only a single executor on
 /// core 0
-pub fn spawm_on_core<T, F>(
+pub fn spawn_on_core<T, F>(
     core: u32,
     arg: Box<T>,
     f: F,

mayastor/src/rebuild.rs
@@ -54,8 +54,8 @@ pub struct RebuildTask {
     /// used to provide progress indication
     previous_lba: u64,
     /// used to signal completion to the callee
-    sender: Option<oneshot::Sender<bool>>,
-    completed: Option<oneshot::Receiver<bool>>,
+    sender: Option<oneshot::Sender<RebuildState>>,
+    completed: Option<oneshot::Receiver<RebuildState>>,
     /// progress reported to logs
     progress: Option<PollTask>,
     /// the number of segments we need to rebuild. The segment is derived from
@@ -106,7 +106,7 @@ impl RebuildTask {
                 as usize,
         )?;
 
-        let (s, r) = oneshot::channel::<bool>();
+        let (s, r) = oneshot::channel::<RebuildState>();
         let task = Box::new(Self {
             state: RebuildState::Initialized,
             blocks_per_segment: blocks_per_segment as u32,
@@ -157,7 +157,9 @@ impl RebuildTask {
         }
     }
 
-    pub async fn completed(&mut self) -> Result<bool, oneshot::Canceled> {
+    pub async fn completed(
+        &mut self,
+    ) -> Result<RebuildState, oneshot::Canceled> {
         self.completed.as_mut().unwrap().await
     }
 
@@ -252,7 +254,7 @@ impl RebuildTask {
 
         let _ = self.progress.take();
 
-        self.send_completion(success);
+        self.send_completion(self.state);
     }
 
     /// determine the next segment for which we will issue a rebuild
@@ -355,9 +357,9 @@ impl RebuildTask {
     }
 
     /// send the callee that we completed successfully
-    fn send_completion(&mut self, success: bool) {
+    fn send_completion(&mut self, state: RebuildState) {
         if let Some(sender) = self.sender.take() {
-            let _ = sender.send(success);
+            let _ = sender.send(state);
         }
     }
 

commit 03b2cfbb8944425e92827db6af634f9335f69b18

nexus: Close child when destroying all Nexus children
Make Nexus::destroy_children close a child first before destroying it,
making its behaviour match the code comment.

Addresses this panic when creating a Nexus with multiple children
where the 2nd or later child is inaccessible:

thread 'main' panicked at 'assertion failed: `(left == right)`
  left: `Init`,
 right: `Closed`', mayastor/src/bdev/nexus/nexus_child.rs:218:9

Fixes: 63bd078 ("nexus: add more test cases for adding children")

Resolves: CAS-297

mayastor/src/bdev/nexus/nexus_bdev_children.rs
@@ -434,7 +434,10 @@ impl Nexus {
     /// destroy all children that are part of this nexus closes any child
     /// that might be open first
     pub(crate) async fn destroy_children(&mut self) {
-        let futures = self.children.iter_mut().map(|c| c.destroy());
+        let futures = self.children.iter_mut().map(|c| {
+            c.close();
+            c.destroy()
+        });
         let results = join_all(futures).await;
         if results.iter().any(|c| c.is_err()) {
             error!("{}: Failed to destroy child", self.name);

mayastor/tests/core.rs
@@ -12,6 +12,7 @@ use mayastor::{
 };
 use rpc::mayastor::ShareProtocolNexus;
 use std::sync::Once;
+use uuid::Uuid;
 
 static DISKNAME1: &str = "/tmp/disk1.img";
 static BDEVNAME1: &str = "aio:///tmp/disk1.img?blk_size=512";
@@ -243,6 +244,24 @@ fn core_5() {
 
         common::delete_file(&[DISKNAME1.to_string()]);
     }
+}
+
+#[test]
+// Test nexus with inaccessible bdev for 2nd child
+fn core_6() {
+    test_init!();
+    common::truncate_file(DISKNAME1, 64 * 1024);
+
+    let file_uuid = Uuid::new_v4();
+    let ch = vec![
+        BDEVNAME1.to_string(),
+        "aio:///tmp/disk2".to_string() + &file_uuid.to_simple().to_string(),
+    ];
+    Reactor::block_on(async move {
+        nexus_create("nexus_child_2_missing", 64 * 1024 * 1024, None, &ch)
+            .await
+            .expect_err("Nexus should not be created");
+    });
 
     mayastor_env_stop(1);
 }