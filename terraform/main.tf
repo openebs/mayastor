dea0b0177c57b11f78b362fc7f6800a59e23a943
Tiago Castro <tiagolobocastro@gmail.com>
2020-06-17 18:31:37 +0100

    fix broken terraform lxd (#284)
    the node.sh seems to exit on error now which fails for lxd
    fix: don't change the hugepages or modprobe modules for lxd only
    
    also simplify main.tf so that only one line is required to swap out in
    order to switch from lxd to libvirt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,32 @@
-module "libvirt" {
-  source = "./mod/libvirt"
-
-  image_path         = var.image_path
-  num_nodes          = var.num_nodes
-  hostname_formatter = var.hostname_formatter
-  ssh_user           = var.ssh_user
-  ssh_key            = var.ssh_key
-  private_key_path   = var.private_key_path
-  disk_size          = var.disk_size
-  qcow2_image        = var.qcow2_image
-}
-
-#module "lxd" {
-#  source   = "./mod/lxd"
-#  ssh_user = var.ssh_user
-#  ssh_key  = var.ssh_key
-#  num_nodes = var.num_nodes
-#}
-
 module "k8s" {
   source = "./mod/k8s"
 
   num_nodes = var.num_nodes
   ssh_user  = var.ssh_user
   private_key_path = var.private_key_path
-  #node_list = module.lxd.node_list
-  node_list = module.libvirt.node_list
+  node_list =  module.provider.node_list
   overlay_cidr = var.overlay_cidr
   nr_hugepages = var.nr_hugepages
   modprobe_nvme = var.modprobe_nvme
 }
 
+module "provider" {
+  #source = "./mod/lxd"
+  source = "./mod/libvirt"
+
+  # lxd and libvirt
+  ssh_user  = var.ssh_user
+  ssh_key   = var.ssh_key
+  num_nodes = var.num_nodes
+
+  # libvirt
+  image_path         = var.image_path
+  hostname_formatter = var.hostname_formatter
+  private_key_path   = var.private_key_path
+  disk_size          = var.disk_size
+  qcow2_image        = var.qcow2_image
+}
+
 output "kluster" {
- 	value =  module.libvirt.ks-cluster-nodes
+  value =  module.provider.ks-cluster-nodes
 }
