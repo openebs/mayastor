388443d899383fc8dee155cc2c8d1599741a0826
Jeffry Molanus <jeffry.molanus@gmail.com>
2020-06-25 11:51:56 +0200

    nix: add image creation
    To produce images we can run:
    
    nix-build -A images.csi
    nix-build -A images.mayastor-image
    
    To create a image an adhoc image (i.e after cargo build)
    
    nix-build -A image.quick
    
    The later will wrap the binaries in target/debug into a docker
    image for quick and easy testing

--- nix/mayastor-overlay.nix
+++ nix/mayastor-overlay.nix
@@ -5,11 +5,13 @@ self: super: {
   nvmet-cli = super.callPackage ./pkgs/nvmet-cli { };
   libspdk = super.callPackage ./pkgs/libspdk { };
   mayastor = super.callPackage ./pkgs/mayastor { };
-  mayastorImage = (super.callPackage ./pkgs/mayastor { }).mayastorImage;
-  mayastorCSIImage = (super.callPackage ./pkgs/mayastor { }).mayastorCSIImage;
-  mayastor-docker = (super.callPackage ./pkgs/images { }).mayastor-dev;
+  images = super.callPackage ./pkgs/images { };
+
+
   ms-buildenv = super.callPackage ./pkgs/ms-buildenv { };
   mkContainerEnv = super.callPackage ./lib/mkContainerEnv.nix { };
+
+
   node-moac = (import ./../csi/moac { pkgs = super; }).package;
   node-moacImage = (import ./../csi/moac { pkgs = super; }).buildImage;
   nodePackages = (import ./pkgs/nodePackages { pkgs = super; });
