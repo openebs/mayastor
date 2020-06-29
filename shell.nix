22a58668433983f32b12c079ac9a6d2e4c47ce02
Jeffry Molanus <jeffry.molanus@gmail.com>
2020-06-29 15:54:15 +0200

    CI: use node/node12 for nodejs linting

--- shell.nix
+++ shell.nix
@@ -18,7 +18,6 @@ mkShell {
   # fortify does not work with -O0 which is used by spdk when --enable-debug
   hardeningDisable = [ "fortify" ];
   buildInputs = [
-    #    nats-server
     channel.nightly.rust
     clang
     cowsay
@@ -31,6 +30,7 @@ mkShell {
     libiscsi.bin
     libudev
     llvmPackages.libclang
+    nats-server
     nodejs-12_x
     numactl
     nvme-cli
