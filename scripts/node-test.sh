3c3980e0cb7c2c2adadede12515b33adacda016d
Jeffry Molanus <jeffry.molanus@gmail.com>
2020-06-29 14:17:25 +0200

    CI: add back csi test

--- scripts/node-test.sh
+++ scripts/node-test.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 export PATH=$PATH:${HOME}/.cargo/bin
 cargo build --all
 ( cd mayastor-test && npm install && ./node_modules/mocha/bin/mocha test_cli.js )
+( cd mayastor-test && ./node_modules/mocha/bin/mocha test_csi.js )
 ( cd mayastor-test && ./node_modules/mocha/bin/mocha test_replica.js )
 ( cd mayastor-test && ./node_modules/mocha/bin/mocha test_nexus.js )
 ( cd mayastor-test && ./node_modules/mocha/bin/mocha test_rebuild.js )
