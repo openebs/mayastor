a4c30a295e22b37a98092fff49102ab0f806b2b1
Jan Kryl <jan.kryl@mayadata.io>
2020-07-29 14:26:00 +0000

    Give users chance to use their own rust version in nix-shell.
    Example usage: nix-shell --arg norust true
    
    Update mozilla-pkgs overlay in order to get the fix for
    https://github.com/mozilla/nixpkgs-mozilla/issues/225 and enable us
    to update rust nightly to latest snapshot (done on behalf of this
    commit too).

--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
-{ nospdk ? false }:
+{ nospdk ? false
+, norust ? false
+}:
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {
@@ -10,7 +12,8 @@ let
 in
 with pkgs;
 let
-  moth = "You have requested environment without SPDK, you should provide it!";
+  nospdk_moth = "You have requested environment without SPDK, you should provide it!";
+  norust_moth = "You have requested environment without rust, you should provide it!";
   channel = import ./nix/lib/rust.nix { inherit sources; };
 in
 mkShell {
@@ -18,7 +21,6 @@ mkShell {
   # fortify does not work with -O0 which is used by spdk when --enable-debug
   hardeningDisable = [ "fortify" ];
   buildInputs = [
-    channel.nightly.rust
     clang
     cowsay
     e2fsprogs
@@ -41,18 +43,24 @@ mkShell {
     python3
     utillinux
     xfsprogs
-  ] ++ pkgs.lib.optionals (nospdk) libspdk.buildInputs
-  ++ pkgs.lib.optional (!nospdk) libspdk;
+  ]
+  ++ (if (nospdk) then [ libspdk.buildInputs ] else [ libspdk ])
+  ++ pkgs.lib.optional (!norust) channel.nightly.rust;
+
   LIBCLANG_PATH = mayastor.LIBCLANG_PATH;
   PROTOC = mayastor.PROTOC;
   PROTOC_INCLUDE = mayastor.PROTOC_INCLUDE;
-
   SPDK_PATH = if nospdk then null else "${libspdk}";
+
   shellHook = ''
-    ${pkgs.lib.optionalString (nospdk) "cowsay ${moth}"}
+    ${pkgs.lib.optionalString (nospdk) "cowsay ${nospdk_moth}"}
     ${pkgs.lib.optionalString (nospdk) "export CFLAGS=-msse4"}
     ${pkgs.lib.optionalString (nospdk)
       ''export RUSTFLAGS="-C link-args=-Wl,-rpath,$(pwd)/spdk-sys/spdk"''}
+    ${pkgs.lib.optionalString (nospdk) "echo"}
+    ${pkgs.lib.optionalString (norust) "cowsay ${norust_moth}"}
+    ${pkgs.lib.optionalString (norust) "echo 'Hint: use rustup tool.'"}
+    ${pkgs.lib.optionalString (norust) "echo"}
     pre-commit install
   '';
 }
