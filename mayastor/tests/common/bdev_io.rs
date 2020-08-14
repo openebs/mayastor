openebs/mayastor

commit d909c1eb67d8780db977270fb6bc360ab35be2ae

replica: Implement create_snapshot
This implements basic functionality to allow a Nexus to create
a snapshot on a replica using an NVMe Admin command.

Add NVME_ADMIN as a permitted IO type and implement NVMe Admin
Passthru starting from BdevHandle as a standalone function until it
reaches nexus_bdev. Creating a snapshot sends an admin command with
opcode c0h and the current Unix time in cdw10/11.

Register a custom command handler for NVMe Admin opcode c0h with SPDK
to handle snapshot creation when initialising the NVMf subsystem.
The handler creates a snapshot against the replica (lvol) by calling
vbdev_lvol_create_snapshot with a snapshot name derived from the
timestamp. Complete the NVMf request in the snapshot done callback.
The snapshot is then available in the list of replicas.

Add a test of NVMe Admin commands first with an unsupported opcode,
followed by creating a snapshot, given a BdevHandle, on a Nexus.

mayastor/src/bdev/nexus/nexus_bdev.rs
@@ -22,6 +22,7 @@ use spdk_sys::{
     spdk_bdev_flush_blocks,
     spdk_bdev_io,
     spdk_bdev_io_get_buf,
+    spdk_bdev_nvme_admin_passthru,
     spdk_bdev_readv_blocks,
     spdk_bdev_register,
     spdk_bdev_reset,
@@ -32,6 +33,7 @@ use spdk_sys::{
     spdk_io_channel,
     spdk_io_device_register,
     spdk_io_device_unregister,
+    spdk_nvmf_request,
 };
 
 use crate::{
@@ -41,7 +43,7 @@ use crate::{
             instances,
             nexus_channel::{DREvent, NexusChannel, NexusChannelInner},
             nexus_child::{ChildError, ChildState, ChildStatus, NexusChild},
-            nexus_io::{io_status, Bio},
+            nexus_io::{io_status, nvme_admin_opc, Bio},
             nexus_iscsi::{NexusIscsiError, NexusIscsiTarget},
             nexus_label::LabelError,
             nexus_nbd::{NbdDisk, NbdError},
@@ -52,6 +54,7 @@ use crate::{
     ffihelper::errno_result_from_i32,
     nexus_uri::{bdev_destroy, NexusBdevError},
     rebuild::RebuildError,
+    replica::Replica,
 };
 
 /// Obtain the full error chain
@@ -372,6 +375,60 @@ impl Drop for Nexus {
     }
 }
 
+extern "C" fn nvmf_create_snapshot_hdlr(req: *mut spdk_nvmf_request) -> i32 {
+    debug!("nvmf_create_snapshot_hdlr {:?}", req);
+
+    let subsys = unsafe { spdk_sys::spdk_nvmf_request_get_subsystem(req) };
+    if subsys.is_null() {
+        debug!("subsystem is null");
+        return -1;
+    }
+
+    /* Only process this request if it has exactly one namespace */
+    if unsafe { spdk_sys::spdk_nvmf_subsystem_get_max_nsid(subsys) } != 1 {
+        debug!("multiple namespaces");
+        return -1;
+    }
+
+    /* Forward to first namespace if it supports NVME admin commands */
+    let mut bdev: *mut spdk_bdev = std::ptr::null_mut();
+    let mut desc: *mut spdk_bdev_desc = std::ptr::null_mut();
+    let mut ch: *mut spdk_io_channel = std::ptr::null_mut();
+    let rc = unsafe {
+        spdk_sys::spdk_nvmf_request_get_bdev(
+            1, req, &mut bdev, &mut desc, &mut ch,
+        )
+    };
+    if rc != 0 {
+        /* No bdev found for this namespace. Continue. */
+        debug!("no bdev found");
+        return -1;
+    }
+
+    let bd = Bdev::from(bdev);
+    if let Some(replica) = Replica::from_bdev(&bd) {
+        let cmd = unsafe { &*spdk_sys::spdk_nvmf_request_get_cmd(req) };
+        let snapshot_time = unsafe {
+            cmd.__bindgen_anon_1.cdw10 as u64
+                | (cmd.__bindgen_anon_2.cdw11 as u64) << 32
+        };
+        let snapshot_name = format!("{}-snap-{}", bd.name(), snapshot_time);
+        replica.create_snapshot(req, &snapshot_name);
+        1 // SPDK_NVMF_REQUEST_EXEC_STATUS_ASYNCHRONOUS
+    } else {
+        -1
+    }
+}
+
+pub fn setup_create_snapshot_hdlr() {
+    unsafe {
+        spdk_sys::spdk_nvmf_set_custom_admin_cmd_hdlr(
+            nvme_admin_opc::CREATE_SNAPSHOT,
+            Some(nvmf_create_snapshot_hdlr),
+        );
+    }
+}
+
 impl Nexus {
     /// create a new nexus instance with optionally directly attaching
     /// children to it.
@@ -613,7 +670,7 @@ impl Nexus {
     }
 
     /// determine if any of the children do not support the requested
-    /// io type. Brake the loop on first occurrence.
+    /// io type. Break the loop on first occurrence.
     /// TODO: optionally add this check during nexus creation
     pub fn io_is_supported(&self, io_type: u32) -> bool {
         self.children
@@ -884,6 +941,45 @@ impl Nexus {
                     c,
                     io.offset() + io.nexus_as_ref().data_ent_offset,
                     io.num_blocks(),
+                    Some(Self::io_completion),
+                    pio as *mut _,
+                )
+            })
+            .collect::<Vec<_>>();
+
+        if results.iter().any(|r| *r != 0) {
+            error!(
+                "{}: Failed to submit dispatched IO {:?}",
+                io.nexus_as_ref().name,
+                pio
+            );
+        }
+    }
+
+    pub(crate) fn nvme_admin(
+        &self,
+        pio: *mut spdk_bdev_io,
+        channels: &NexusChannelInner,
+    ) {
+        let mut io = Bio(pio);
+        if io.nvme_cmd().opc() == nvme_admin_opc::CREATE_SNAPSHOT as u16 {
+            // FIXME: pause IO before dispatching
+            debug!("Passing thru create snapshot as NVMe Admin command");
+        }
+        // for pools, pass thru only works with our vendor commands as the
+        // underlying bdev is not nvmf
+        io.ctx_as_mut_ref().in_flight = channels.ch.len() as i8;
+        let results = channels
+            .ch
+            .iter()
+            .map(|c| unsafe {
+                let (desc, chan) = c.io_tuple();
+                spdk_bdev_nvme_admin_passthru(
+                    desc,
+                    chan,
+                    &io.nvme_cmd(),
+                    io.nvme_buf(),
+                    io.nvme_nbytes(),
                     Some(Self::io_completion),
                     pio as *mut _,
                 )

mayastor/src/core/handle.rs
@@ -4,6 +4,7 @@ use std::{
     mem::ManuallyDrop,
     os::raw::c_void,
     sync::Arc,
+    time::{SystemTime, UNIX_EPOCH},
 };
 
 use futures::channel::oneshot;
@@ -14,6 +15,7 @@ use spdk_sys::{
     spdk_bdev_desc,
     spdk_bdev_free_io,
     spdk_bdev_io,
+    spdk_bdev_nvme_admin_passthru,
     spdk_bdev_read,
     spdk_bdev_reset,
     spdk_bdev_write,
@@ -21,6 +23,7 @@ use spdk_sys::{
 };
 
 use crate::{
+    bdev::nexus::nexus_io::nvme_admin_opc,
     core::{Bdev, CoreError, Descriptor, DmaBuf, DmaError, IoChannel},
     ffihelper::cb_arg,
 };
@@ -191,6 +194,63 @@ impl BdevHandle {
             Ok(0)
         } else {
             Err(CoreError::ResetFailed {})
+        }
+    }
+
+    pub async fn create_snapshot(&self) -> Result<usize, CoreError> {
+        let mut cmd = spdk_sys::spdk_nvme_cmd::default();
+        cmd.set_opc(nvme_admin_opc::CREATE_SNAPSHOT.into());
+        // Snapshot time as u64 seconds since Unix epoch encoded in cdw10/11
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        cmd.__bindgen_anon_1.cdw10 = now as u32;
+        cmd.__bindgen_anon_2.cdw11 = (now >> 32) as u32;
+        debug!("Creating snapshot at {}", now);
+        self.nvme_admin(&cmd).await
+    }
+
+    pub async fn nvme_admin_custom(
+        &self,
+        opcode: u8,
+    ) -> Result<usize, CoreError> {
+        let mut cmd = spdk_sys::spdk_nvme_cmd::default();
+        cmd.set_opc(opcode.into());
+        self.nvme_admin(&cmd).await
+    }
+
+    pub async fn nvme_admin(
+        &self,
+        nvme_cmd: &spdk_sys::spdk_nvme_cmd,
+    ) -> Result<usize, CoreError> {
+        trace!("Sending nvme_admin {}", nvme_cmd.opc());
+        let (s, r) = oneshot::channel::<bool>();
+        let errno = unsafe {
+            spdk_bdev_nvme_admin_passthru(
+                self.desc.as_ptr(),
+                self.channel.as_ptr(),
+                &*nvme_cmd,
+                std::ptr::null_mut(),
+                0,
+                Some(Self::io_completion_cb),
+                cb_arg(s),
+            )
+        };
+
+        if errno != 0 {
+            return Err(CoreError::NvmeAdminDispatch {
+                source: Errno::from_i32(errno),
+                opcode: (*nvme_cmd).opc(),
+            });
+        }
+
+        if r.await.expect("Failed awaiting NVME Admin IO") {
+            Ok(0)
+        } else {
+            Err(CoreError::NvmeAdminFailed {
+                opcode: (*nvme_cmd).opc(),
+            })
         }
     }
 }

commit 33d46bfc0602f413a85066c48be533950a6c9a9a

snapshots: Minor refactoring
Move NVMe admin command handler to the nvmf subsystem.

In Replica, move the snapshot done callback to the function that uses
it, refactor use of nvme_status, and use ffihelper for conversion to a
C string.

Add function comments where appropriate.

In the integration tests, factor out read_some and write_some to
common and swtich current users to that.

Fixes: d909c1e ("replica: Implement create_snapshot")

mayastor/src/bdev/nexus/nexus_bdev.rs
@@ -33,7 +33,6 @@ use spdk_sys::{
     spdk_io_channel,
     spdk_io_device_register,
     spdk_io_device_unregister,
-    spdk_nvmf_request,
 };
 
 use crate::{
@@ -54,7 +53,6 @@ use crate::{
     ffihelper::errno_result_from_i32,
     nexus_uri::{bdev_destroy, NexusBdevError},
     rebuild::RebuildError,
-    replica::Replica,
 };
 
 /// Obtain the full error chain
@@ -372,60 +370,6 @@ impl Drop for Nexus {
             let _ = std::ffi::CString::from_raw(b.name);
             let _ = std::ffi::CString::from_raw(b.product_name);
         }
-    }
-}
-
-extern "C" fn nvmf_create_snapshot_hdlr(req: *mut spdk_nvmf_request) -> i32 {
-    debug!("nvmf_create_snapshot_hdlr {:?}", req);
-
-    let subsys = unsafe { spdk_sys::spdk_nvmf_request_get_subsystem(req) };
-    if subsys.is_null() {
-        debug!("subsystem is null");
-        return -1;
-    }
-
-    /* Only process this request if it has exactly one namespace */
-    if unsafe { spdk_sys::spdk_nvmf_subsystem_get_max_nsid(subsys) } != 1 {
-        debug!("multiple namespaces");
-        return -1;
-    }
-
-    /* Forward to first namespace if it supports NVME admin commands */
-    let mut bdev: *mut spdk_bdev = std::ptr::null_mut();
-    let mut desc: *mut spdk_bdev_desc = std::ptr::null_mut();
-    let mut ch: *mut spdk_io_channel = std::ptr::null_mut();
-    let rc = unsafe {
-        spdk_sys::spdk_nvmf_request_get_bdev(
-            1, req, &mut bdev, &mut desc, &mut ch,
-        )
-    };
-    if rc != 0 {
-        /* No bdev found for this namespace. Continue. */
-        debug!("no bdev found");
-        return -1;
-    }
-
-    let bd = Bdev::from(bdev);
-    if let Some(replica) = Replica::from_bdev(&bd) {
-        let cmd = unsafe { &*spdk_sys::spdk_nvmf_request_get_cmd(req) };
-        let snapshot_time = unsafe {
-            cmd.__bindgen_anon_1.cdw10 as u64
-                | (cmd.__bindgen_anon_2.cdw11 as u64) << 32
-        };
-        let snapshot_name = format!("{}-snap-{}", bd.name(), snapshot_time);
-        replica.create_snapshot(req, &snapshot_name);
-        1 // SPDK_NVMF_REQUEST_EXEC_STATUS_ASYNCHRONOUS
-    } else {
-        -1
-    }
-}
-
-pub fn setup_create_snapshot_hdlr() {
-    unsafe {
-        spdk_sys::spdk_nvmf_set_custom_admin_cmd_hdlr(
-            nvme_admin_opc::CREATE_SNAPSHOT,
-            Some(nvmf_create_snapshot_hdlr),
-        );
     }
 }
 

mayastor/src/core/handle.rs
@@ -197,6 +197,7 @@ impl BdevHandle {
         }
     }
 
+    /// create a snapshot on all children
     pub async fn create_snapshot(&self) -> Result<usize, CoreError> {
         let mut cmd = spdk_sys::spdk_nvme_cmd::default();
         cmd.set_opc(nvme_admin_opc::CREATE_SNAPSHOT.into());
@@ -211,6 +212,7 @@ impl BdevHandle {
         self.nvme_admin(&cmd).await
     }
 
+    /// sends an NVMe Admin command with a custom opcode to all children
     pub async fn nvme_admin_custom(
         &self,
         opcode: u8,
@@ -220,6 +222,7 @@ impl BdevHandle {
         self.nvme_admin(&cmd).await
     }
 
+    /// sends the specified NVMe Admin command to all children
     pub async fn nvme_admin(
         &self,
         nvme_cmd: &spdk_sys::spdk_nvme_cmd,

mayastor/src/replica.rs
@@ -13,6 +13,7 @@ use snafu::{ResultExt, Snafu};
 use spdk_sys::{
     spdk_lvol,
     spdk_nvme_cpl,
+    spdk_nvme_status,
     spdk_nvmf_request,
     vbdev_lvol_create,
     vbdev_lvol_create_snapshot,
@@ -25,7 +26,13 @@ use spdk_sys::{
 
 use crate::{
     core::Bdev,
-    ffihelper::{cb_arg, done_errno_cb, errno_result_from_i32, ErrnoResult},
+    ffihelper::{
+        cb_arg,
+        done_errno_cb,
+        errno_result_from_i32,
+        ErrnoResult,
+        IntoCString,
+    },
     pool::Pool,
     subsys::NvmfSubsystem,
     target,
@@ -279,12 +286,42 @@ impl Replica {
         nvmf_req: *mut spdk_nvmf_request,
         snapshot_name: &str,
     ) {
-        let c_snapshot_name = CString::new(snapshot_name).unwrap();
+        extern "C" fn snapshot_done_cb(
+            nvmf_req_ptr: *mut c_void,
+            _lvol_ptr: *mut spdk_lvol,
+            errno: i32,
+        ) {
+            let rsp: &mut spdk_nvme_cpl = unsafe {
+                &mut *spdk_sys::spdk_nvmf_request_get_response(
+                    nvmf_req_ptr as *mut spdk_nvmf_request,
+                )
+            };
+            let nvme_status: &mut spdk_nvme_status =
+                unsafe { &mut rsp.__bindgen_anon_1.status };
+
+            nvme_status.set_sct(0); // SPDK_NVME_SCT_GENERIC
+            nvme_status.set_sc(match errno {
+                0 => 0,
+                _ => {
+                    debug!("vbdev_lvol_create_snapshot errno {}", errno);
+                    0x06 // SPDK_NVME_SC_INTERNAL_DEVICE_ERROR
+                }
+            });
+
+            // From nvmf_bdev_ctrlr_complete_cmd
+            unsafe {
+                spdk_sys::spdk_nvmf_request_complete(
+                    nvmf_req_ptr as *mut spdk_nvmf_request,
+                );
+            }
+        }
+
+        let c_snapshot_name = snapshot_name.into_cstring();
         unsafe {
             vbdev_lvol_create_snapshot(
                 self.as_ptr(),
                 c_snapshot_name.as_ptr(),
-                Some(Self::snapshot_done_cb),
+                Some(snapshot_done_cb),
                 nvmf_req as *mut c_void,
             )
         };
@@ -393,37 +430,6 @@ impl Replica {
         sender
             .send(errno_result_from_i32(lvol_ptr, errno))
             .expect("Receiver is gone");
-    }
-
-    /// Callback called from SPDK for snapshot create method.
-    extern "C" fn snapshot_done_cb(
-        nvmf_req_ptr: *mut c_void,
-        _lvol_ptr: *mut spdk_lvol,
-        errno: i32,
-    ) {
-        let rsp: &mut spdk_nvme_cpl = unsafe {
-            &mut *spdk_sys::spdk_nvmf_request_get_response(
-                nvmf_req_ptr as *mut spdk_nvmf_request,
-            )
-        };
-
-        unsafe {
-            rsp.__bindgen_anon_1.status.set_sct(0); // SPDK_NVME_SCT_GENERIC
-            rsp.__bindgen_anon_1.status.set_sc(match errno {
-                0 => 0,
-                _ => {
-                    debug!("vbdev_lvol_create_snapshot errno {}", errno);
-                    0x06 // SPDK_NVME_SC_INTERNAL_DEVICE_ERROR
-                }
-            })
-        }
-
-        // From nvmf_bdev_ctrlr_complete_cmd
-        unsafe {
-            spdk_sys::spdk_nvmf_request_complete(
-                nvmf_req_ptr as *mut spdk_nvmf_request,
-            );
-        }
     }
 }
 

mayastor/src/subsys/nvmf/admin_cmd.rs
@@ -1,0 +1,67 @@
+//! Handlers for custom NVMe Admin commands
+
+use spdk_sys::{spdk_bdev, spdk_bdev_desc, spdk_io_channel, spdk_nvmf_request};
+
+use crate::{
+    bdev::nexus::nexus_io::nvme_admin_opc,
+    core::Bdev,
+    replica::Replica,
+};
+
+/// NVMf custom command handler for opcode c0h
+/// Called from nvmf_ctrlr_process_admin_cmd
+/// Return: <0 for any error, caller handles it as unsupported opcode
+extern "C" fn nvmf_create_snapshot_hdlr(req: *mut spdk_nvmf_request) -> i32 {
+    debug!("nvmf_create_snapshot_hdlr {:?}", req);
+
+    let subsys = unsafe { spdk_sys::spdk_nvmf_request_get_subsystem(req) };
+    if subsys.is_null() {
+        debug!("subsystem is null");
+        return -1;
+    }
+
+    /* Only process this request if it has exactly one namespace */
+    if unsafe { spdk_sys::spdk_nvmf_subsystem_get_max_nsid(subsys) } != 1 {
+        debug!("multiple namespaces");
+        return -1;
+    }
+
+    /* Forward to first namespace if it supports NVME admin commands */
+    let mut bdev: *mut spdk_bdev = std::ptr::null_mut();
+    let mut desc: *mut spdk_bdev_desc = std::ptr::null_mut();
+    let mut ch: *mut spdk_io_channel = std::ptr::null_mut();
+    let rc = unsafe {
+        spdk_sys::spdk_nvmf_request_get_bdev(
+            1, req, &mut bdev, &mut desc, &mut ch,
+        )
+    };
+    if rc != 0 {
+        /* No bdev found for this namespace. Continue. */
+        debug!("no bdev found");
+        return -1;
+    }
+
+    let bd = Bdev::from(bdev);
+    if let Some(replica) = Replica::from_bdev(&bd) {
+        let cmd = unsafe { &*spdk_sys::spdk_nvmf_request_get_cmd(req) };
+        let snapshot_time = unsafe {
+            cmd.__bindgen_anon_1.cdw10 as u64
+                | (cmd.__bindgen_anon_2.cdw11 as u64) << 32
+        };
+        let snapshot_name = format!("{}-snap-{}", bd.name(), snapshot_time);
+        replica.create_snapshot(req, &snapshot_name);
+        1 // SPDK_NVMF_REQUEST_EXEC_STATUS_ASYNCHRONOUS
+    } else {
+        -1
+    }
+}
+
+/// Register custom NVMe admin command handler
+pub fn setup_create_snapshot_hdlr() {
+    unsafe {
+        spdk_sys::spdk_nvmf_set_custom_admin_cmd_hdlr(
+            nvme_admin_opc::CREATE_SNAPSHOT,
+            Some(nvmf_create_snapshot_hdlr),
+        );
+    }
+}

mayastor/src/subsys/nvmf/mod.rs
@@ -23,11 +23,11 @@ pub use subsystem::{NvmfSubsystem, SubType};
 pub use target::Target;
 
 use crate::{
-    bdev::nexus::nexus_bdev,
     jsonrpc::{Code, RpcErrorCode},
     subsys::{nvmf::target::NVMF_TGT, Config},
 };
 
+mod admin_cmd;
 mod poll_groups;
 mod subsystem;
 mod target;
@@ -87,7 +87,7 @@ impl Nvmf {
         // this code only ever gets run on the first core
 
         // set up custom NVMe Admin command handler
-        nexus_bdev::setup_create_snapshot_hdlr();
+        admin_cmd::setup_create_snapshot_hdlr();
 
         if Config::get().nexus_opts.nvmf_enable {
             NVMF_TGT.with(|tgt| {

mayastor/tests/common/bdev_io.rs
@@ -1,0 +1,34 @@
+use mayastor::core::{BdevHandle, CoreError};
+
+pub async fn write_some(nexus_name: &str) -> Result<(), CoreError> {
+    let h = BdevHandle::open(nexus_name, true, false).unwrap();
+    let mut buf = h.dma_malloc(512).expect("failed to allocate buffer");
+    buf.fill(0xff);
+
+    let s = buf.as_slice();
+    assert_eq!(s[0], 0xff);
+
+    h.write_at(0, &buf).await?;
+    Ok(())
+}
+
+pub async fn read_some(nexus_name: &str) -> Result<(), CoreError> {
+    let h = BdevHandle::open(nexus_name, true, false).unwrap();
+    let mut buf = h.dma_malloc(1024).expect("failed to allocate buffer");
+    let slice = buf.as_mut_slice();
+
+    assert_eq!(slice[0], 0);
+    slice[512] = 0xff;
+    assert_eq!(slice[512], 0xff);
+
+    let len = h.read_at(0, &mut buf).await?;
+    assert_eq!(len, 1024);
+
+    let slice = buf.as_slice();
+
+    for &it in slice.iter().take(512) {
+        assert_eq!(it, 0xff);
+    }
+    assert_eq!(slice[512], 0);
+    Ok(())
+}

mayastor/tests/common/mod.rs
@@ -12,8 +12,10 @@ use mayastor::{
 };
 use spdk_sys::spdk_get_thread;
 
+pub mod bdev_io;
 pub mod error_bdev;
 pub mod ms_exec;
+
 /// call F cnt times, and sleep for a duration between each invocation
 pub fn retry<F, T, E>(mut cnt: u32, timeout: Duration, mut f: F) -> T
 where

mayastor/tests/io.rs
@@ -1,13 +1,8 @@
 use std::process::Command;
 
+use common::bdev_io;
 use mayastor::{
-    core::{
-        mayastor_env_stop,
-        Bdev,
-        MayastorCliArgs,
-        MayastorEnvironment,
-        Reactor,
-    },
+    core::{mayastor_env_stop, MayastorCliArgs, MayastorEnvironment, Reactor},
     nexus_uri::bdev_create,
 };
 
@@ -46,41 +41,7 @@ fn io_test() {
 // only execute one future per reactor loop.
 async fn start() {
     bdev_create(BDEVNAME).await.expect("failed to create bdev");
-    write_some().await;
-    read_some().await;
+    bdev_io::write_some(BDEVNAME).await.unwrap();
+    bdev_io::read_some(BDEVNAME).await.unwrap();
     mayastor_env_stop(0);
-}
-
-async fn write_some() {
-    let bdev = Bdev::lookup_by_name(BDEVNAME).expect("failed to lookup bdev");
-    let d = bdev
-        .open(true)
-        .expect("failed open bdev")
-        .into_handle()
-        .unwrap();
-    let mut buf = d.dma_malloc(512).expect("failed to allocate buffer");
-    buf.fill(0xff);
-
-    let s = buf.as_slice();
-    assert_eq!(s[0], 0xff);
-
-    d.write_at(0, &buf).await.unwrap();
-}
-
-async fn read_some() {
-    let bdev = Bdev::lookup_by_name(BDEVNAME).expect("failed to lookup bdev");
-    let d = bdev.open(false).unwrap().into_handle().unwrap();
-    let mut buf = d.dma_malloc(1024).expect("failed to allocate buffer");
-    let slice = buf.as_mut_slice();
-
-    assert_eq!(slice[0], 0);
-    slice[512] = 0xff;
-    assert_eq!(slice[512], 0xff);
-
-    d.read_at(0, &mut buf).await.unwrap();
-
-    let slice = buf.as_slice();
-
-    assert_eq!(slice[0], 0xff);
-    assert_eq!(slice[512], 0);
 }

mayastor/tests/replica_snapshot.rs
@@ -1,6 +1,6 @@
 use std::{io, io::Write, process::Command, thread, time};
 
-use common::ms_exec::MayastorProcess;
+use common::{bdev_io, ms_exec::MayastorProcess};
 use mayastor::{
     bdev::nexus_create,
     core::{
@@ -100,17 +100,17 @@ fn replica_snapshot() {
 
     Reactor::block_on(async {
         create_nexus().await;
-        write_some().await.unwrap();
+        bdev_io::write_some(NXNAME).await.unwrap();
         custom_nvme_admin(0xc1)
             .await
             .expect_err("unexpectedly succeeded invalid nvme admin command");
-        read_some().await.unwrap();
+        bdev_io::read_some(NXNAME).await.unwrap();
         create_snapshot().await.unwrap();
         // Check that IO to the replica still works after creating a snapshot
         // Checking the snapshot itself is tbd
-        read_some().await.unwrap();
-        write_some().await.unwrap();
-        read_some().await.unwrap();
+        bdev_io::read_some(NXNAME).await.unwrap();
+        bdev_io::write_some(NXNAME).await.unwrap();
+        bdev_io::read_some(NXNAME).await.unwrap();
     });
     mayastor_env_stop(0);
 
@@ -126,39 +126,6 @@ async fn create_nexus() {
     nexus_create(NXNAME, 64 * 1024 * 1024, None, &ch)
         .await
         .unwrap();
-}
-
-async fn write_some() -> Result<(), CoreError> {
-    let h = BdevHandle::open(NXNAME, true, false).unwrap();
-    let mut buf = h.dma_malloc(512).expect("failed to allocate buffer");
-    buf.fill(0xff);
-
-    let s = buf.as_slice();
-    assert_eq!(s[0], 0xff);
-
-    h.write_at(0, &buf).await?;
-    Ok(())
-}
-
-async fn read_some() -> Result<(), CoreError> {
-    let h = BdevHandle::open(NXNAME, true, false).unwrap();
-    let mut buf = h.dma_malloc(1024).expect("failed to allocate buffer");
-    let slice = buf.as_mut_slice();
-
-    assert_eq!(slice[0], 0);
-    slice[512] = 0xff;
-    assert_eq!(slice[512], 0xff);
-
-    let len = h.read_at(0, &mut buf).await?;
-    assert_eq!(len, 1024);
-
-    let slice = buf.as_slice();
-
-    for &it in slice.iter().take(512) {
-        assert_eq!(it, 0xff);
-    }
-    assert_eq!(slice[512], 0);
-    Ok(())
 }
 
 async fn create_snapshot() -> Result<(), CoreError> {

mayastor/tests/replica_timeout.rs
@@ -2,17 +2,10 @@
 
 use std::{thread, time};
 
-use common::ms_exec::MayastorProcess;
+use common::{bdev_io, ms_exec::MayastorProcess};
 use mayastor::{
     bdev::{nexus_create, nexus_lookup},
-    core::{
-        mayastor_env_stop,
-        BdevHandle,
-        CoreError,
-        MayastorCliArgs,
-        MayastorEnvironment,
-        Reactor,
-    },
+    core::{mayastor_env_stop, MayastorCliArgs, MayastorEnvironment, Reactor},
     subsys,
     subsys::Config,
 };
@@ -82,8 +75,8 @@ fn replica_stop_cont() {
 
     Reactor::block_on(async {
         create_nexus(true).await;
-        write_some().await;
-        read_some().await.unwrap();
+        bdev_io::write_some(NXNAME).await.unwrap();
+        bdev_io::read_some(NXNAME).await.unwrap();
         ms.sig_stop();
         let handle = thread::spawn(move || {
             // Sufficiently long to cause a controller reset
@@ -92,11 +85,11 @@ fn replica_stop_cont() {
             ms.sig_cont();
             ms
         });
-        read_some()
+        bdev_io::read_some(NXNAME)
             .await
             .expect_err("should fail read after controller reset");
         ms = handle.join().unwrap();
-        read_some()
+        bdev_io::read_some(NXNAME)
             .await
             .expect("should read again after Nexus child continued");
         nexus_lookup(NXNAME).unwrap().destroy().await.unwrap();
@@ -123,20 +116,20 @@ fn replica_term() {
 
     Reactor::block_on(async {
         create_nexus(false).await;
-        write_some().await;
-        read_some().await.unwrap();
+        bdev_io::write_some(NXNAME).await.unwrap();
+        bdev_io::read_some(NXNAME).await.unwrap();
     });
     ms1.sig_term();
     thread::sleep(time::Duration::from_secs(1));
     Reactor::block_on(async {
-        read_some()
+        bdev_io::read_some(NXNAME)
             .await
             .expect("should read with 1 Nexus child terminated");
     });
     ms2.sig_term();
     thread::sleep(time::Duration::from_secs(1));
     Reactor::block_on(async {
-        read_some()
+        bdev_io::read_some(NXNAME)
             .await
             .expect_err("should fail read with 2 Nexus children terminated");
     });
@@ -160,36 +153,4 @@ async fn create_nexus(single: bool) {
     nexus_create(NXNAME, DISKSIZE_KB * 1024, None, &ch)
         .await
         .unwrap();
-}
-
-async fn write_some() {
-    let bdev = BdevHandle::open(NXNAME, true, false).unwrap();
-    let mut buf = bdev.dma_malloc(512).expect("failed to allocate buffer");
-    buf.fill(0xff);
-
-    let s = buf.as_slice();
-    assert_eq!(s[0], 0xff);
-
-    bdev.write_at(0, &buf).await.unwrap();
-}
-
-async fn read_some() -> Result<(), CoreError> {
-    let bdev = BdevHandle::open(NXNAME, true, false).unwrap();
-    let mut buf = bdev.dma_malloc(1024).expect("failed to allocate buffer");
-    let slice = buf.as_mut_slice();
-
-    assert_eq!(slice[0], 0);
-    slice[512] = 0xff;
-    assert_eq!(slice[512], 0xff);
-
-    let len = bdev.read_at(0, &mut buf).await?;
-    assert_eq!(len, 1024);
-
-    let slice = buf.as_slice();
-
-    for &it in slice.iter().take(512) {
-        assert_eq!(it, 0xff);
-    }
-    assert_eq!(slice[512], 0);
-    Ok(())
 }