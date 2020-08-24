731168061c2c35cffe44f47d95d36651a95aed3d
Jeffry Molanus <jeffry.molanus@gmail.com>
2020-08-24 21:58:26 +0200

    lvs: remove the None protocol variant
    If we receive an invalid value over gRPC, we should panic as something
    is very wrong. This avoids leaking invalid value handling down the
    stack.

--- mayastor/src/core/share.rs
+++ mayastor/src/core/share.rs
@@ -3,20 +3,29 @@ use pin_utils::core_reexport::fmt::Formatter;
 use std::fmt::Display;
 
 #[derive(Debug, PartialOrd, PartialEq)]
+/// Indicates what protocol the bdev is shared as
 pub enum Protocol {
-    None,
+    /// not shared by any of the variants
+    Off,
+    /// shared as NVMe-oF TCP
     Nvmf,
+    /// shared as iSCSI
     Iscsi,
-    Invalid,
 }
 
 impl From<i32> for Protocol {
     fn from(p: i32) -> Self {
         match p {
-            0 => Self::None,
+            0 => Self::Off,
             1 => Self::Nvmf,
             2 => Self::Iscsi,
-            _ => Self::Invalid,
+            // we have to handle the whole range of u32  here
+            // We panic here because the gRPC interface should
+            // have never deserialized into something that is invalid. A
+            // different approach is would be to set this to
+            // something that is invalid but that would
+            // open the flood gates, to much and leak into the code base.
+            _ => panic!("invalid share value"),
         }
     }
 }
@@ -24,10 +33,9 @@ impl From<i32> for Protocol {
 impl Display for Protocol {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let p = match self {
-            Self::None => "Not shared",
+            Self::Off => "Not shared",
             Self::Iscsi => "iSCSI",
             Self::Nvmf => "NVMe-oF TCP",
-            Self::Invalid => "Invalid protocol",
         };
         write!(f, "{}", p)
     }
