46b578feb295a9271ca563dbdccc16647b18cf99
Jan Kryl <jan.kryl@mayadata.io>
2020-09-02 15:05:31 +0000

    New debug package libspdk-dev.
    We used "debugEnable" parameter passed to libspdk's default.nix, but
    that did not work quite well. Sometimes it could have happened that
    both debug and production derivations had the same output hash which
    lead to unpredictable build results (don't ask me how). By having two
    packages (one for production and another for debugging) it became
    clearer which one is used where.
    
    Notable change is that nix-shell starts with libspdk-dev which is what
    most people expect for development environment.
    
    snapshot test is removed because it triggers assertion in debug version
    of libspdk. It can be reintroduced when the bug in mayastor is fixed.

--- nix/mayastor-overlay.nix
+++ nix/mayastor-overlay.nix
@@ -2,7 +2,8 @@ self: super: {
   libiscsi = super.callPackage ./pkgs/libiscsi { };
   liburing = super.callPackage ./pkgs/liburing { };
   nvmet-cli = super.callPackage ./pkgs/nvmet-cli { };
-  libspdk = super.callPackage ./pkgs/libspdk { };
+  libspdk = (super.callPackage ./pkgs/libspdk { }).release;
+  libspdk-dev = (super.callPackage ./pkgs/libspdk { }).debug;
   mayastor = (super.callPackage ./pkgs/mayastor { }).release;
   mayastor-dev = (super.callPackage ./pkgs/mayastor { }).debug;
   mayastor-adhoc = (super.callPackage ./pkgs/mayastor { }).adhoc;
--- nix/pkgs/libspdk/default.nix
+++ nix/pkgs/libspdk/default.nix
@@ -1,10 +1,7 @@
 { binutils
-, callPackage
 , cunit
-, enableDebug ? false
 , fetchFromGitHub
 , pkgconfig
-, git
 , lcov
 , libaio
 , libiscsi
@@ -17,132 +14,123 @@
 , numactl
 , openssl
 , python3
-, rdma-core
 , stdenv
 }:
-with stdenv.lib;
-stdenv.mkDerivation rec {
+let
+  # Derivation attributes for production version of libspdk
+  drvAttrs = rec {
+    version = "20.07";
 
-  pname = "libspdk";
-  version = "20.07";
+    src = fetchFromGitHub {
+      owner = "openebs";
+      repo = "spdk";
+      rev = "7ea27936afa336b1e4292b6e4069231be8c5ba86";
+      sha256 = "15ah963172jwbsfcp7y1kc2lbjy909ajjmj863a7vxfzh45xdy8v";
+      #sha256 = stdenv.lib.fakeSha256;
+      fetchSubmodules = true;
+    };
 
-  src = fetchFromGitHub {
-    owner = "openebs";
-    repo = "spdk";
-    rev = "7ea27936afa336b1e4292b6e4069231be8c5ba86";
-    sha256 = "15ah963172jwbsfcp7y1kc2lbjy909ajjmj863a7vxfzh45xdy8v";
-    #sha256 = stdenv.lib.fakeSha256;
-    fetchSubmodules = true;
-  };
+    nativeBuildInputs = [
+      meson
+      ninja
+      pkgconfig
+      python3
+    ];
 
-  nativeBuildInputs = [
-    meson
-    ninja
-    pkgconfig
-    python3
-  ];
+    buildInputs = [
+      binutils
+      libaio
+      libiscsi.dev
+      liburing
+      libuuid
+      nasm
+      ncurses
+      numactl
+      openssl
+    ];
 
-  buildInputs = [
-    binutils
-    libaio
-    libiscsi.dev
-    liburing
-    libuuid
-    nasm
-    ncurses
-    numactl
-    openssl
-  ] ++ stdenv.lib.optionals enableDebug [ cunit lcov ];
-
-  # add this once we merged this new option from upstream. The tests are going
-  # to be split up in unit-test and test configure options. test will contain
-  # utilities like bdevperf that is getting more features into it.
-
-  #${enableFeature enableDebug "unit-tests"}
-
-  configureFlags = [
-    "${enableFeature enableDebug "tests"}"
-    "${enableFeature enableDebug "unit-tests"}"
-    "--target-arch=nehalem"
-    "--without-isal"
-    "--without-vhost"
-    "--with-iscsi-initiator"
-    "--with-crypto"
-  ] ++ stdenv.lib.optionals (enableDebug) [ "--enable-debug" ];
+    configureFlags = [
+      "--target-arch=nehalem"
+      "--without-isal"
+      "--without-vhost"
+      "--with-iscsi-initiator"
+      "--with-crypto"
+    ];
 
 
-  enableParallelBuilding = true;
+    enableParallelBuilding = true;
 
-  preConfigure = ''
-    substituteInPlace dpdk/config/defconfig_x86_64-native-linux-gcc --replace native default
-  '';
+    preConfigure = ''
+      substituteInPlace dpdk/config/defconfig_x86_64-native-linux-gcc --replace native default
+    '';
 
-  configurePhase = ''
-    patchShebangs ./.
-    ./configure $configureFlags
-  '';
+    configurePhase = ''
+      patchShebangs ./.
+      ./configure $configureFlags
+    '';
 
-  hardeningDisable = [ "all" ];
+    hardeningDisable = [ "all" ];
 
-  buildPhase = ''
+    buildPhase = ''
       make -j`nproc`
-    find . -type f -name 'libspdk_ut_mock.a' -delete
-    #find . -type f -name 'librte_vhost.a' -delete
+      find . -type f -name 'libspdk_ut_mock.a' -delete
+      #find . -type f -name 'librte_vhost.a' -delete
 
-    $CC -shared -o libspdk.so \
-    -lc  -laio -liscsi -lnuma -ldl -lrt -luuid -lpthread -lcrypto \
-    -Wl,--whole-archive \
-    $(find build/lib -type f -name 'libspdk_*.a*' -o -name 'librte_*.a*') \
-    $(find dpdk/build/lib -type f -name 'librte_*.a*') \
-    $(find intel-ipsec-mb -type f -name 'libIPSec_*.a*') \
-    -Wl,--no-whole-archive
-  '';
+      $CC -shared -o libspdk.so \
+      -lc  -laio -liscsi -lnuma -ldl -lrt -luuid -lpthread -lcrypto \
+      -Wl,--whole-archive \
+      $(find build/lib -type f -name 'libspdk_*.a*' -o -name 'librte_*.a*') \
+      $(find dpdk/build/lib -type f -name 'librte_*.a*') \
+      $(find intel-ipsec-mb -type f -name 'libIPSec_*.a*') \
+      -Wl,--no-whole-archive
+    '';
 
- # postBuild = ''
- #   find . -type f -name 'libspdk_ut_mock.a' -delete
- #   #find . -type f -name 'librte_vhost.a' -delete
+    installPhase = ''
+      mkdir -p $out/lib
+      mkdir $out/bin
 
- #   $CC -shared -o libspdk.so \
- #   -lc  -laio -liscsi -lnuma -ldl -lrt -luuid -lpthread -lcrypto \
- #   -Wl,--whole-archive \
- #   $(find build/lib -type f -name 'libspdk_*.a*' -o -name 'librte_*.a*') \
- #   $(find dpdk/build/lib -type f -name 'librte_*.a*') \
- #   $(find intel-ipsec-mb -type f -name 'libIPSec_*.a*') \
- #   -Wl,--no-whole-archive
+      pushd include
+      find . -type f -name "*.h" -exec install -D "{}" $out/include/{} \;
+      popd
 
- # '';
+      pushd lib
+      find . -type f -name "*.h" -exec install -D "{}" $out/include/spdk/lib/{} \;
+      popd
 
-  installPhase = ''
-    mkdir -p $out/lib
-    mkdir $out/bin
+      # copy private headers from bdev modules needed for creating of bdevs
+      pushd module
+      find . -type f -name "*.h" -exec install -D "{}" $out/include/spdk/module/{} \;
+      popd
 
-    pushd include
-    find . -type f -name "*.h" -exec install -D "{}" $out/include/{} \;
-    popd
+      # copy over the library
+      cp libspdk.so $out/lib
 
-    pushd lib
-    find . -type f -name "*.h" -exec install -D "{}" $out/include/spdk/lib/{} \;
-    popd
-
-    # copy private headers from bdev modules needed for creating of bdevs
-    pushd module
-    find . -type f -name "*.h" -exec install -D "{}" $out/include/spdk/module/{} \;
-    popd
-
-    # copy over the library
-    cp libspdk.so $out/lib
-
-    if [ $enableDebug ]
-    then
-      echo "copying files"
+      echo $(find $out -type f -name '*.a*' -delete)
+      find . -executable -type f -name 'bdevperf' -exec install -D "{}" $out/bin \;
+    '';
+  };
+in
+{
+  release = stdenv.mkDerivation (drvAttrs // {
+    pname = "libspdk";
+    separateDebugInfo = true;
+    dontStrip = false;
+    configureFlags = drvAttrs.configureFlags ++ [
+      "--disable-tests"
+      "--disable-unit-tests"
+    ];
+  });
+  debug = stdenv.mkDerivation (drvAttrs // {
+    pname = "libspdk-dev";
+    separateDebugInfo = false;
+    dontStrip = true;
+    buildInputs = drvAttrs.buildInputs ++ [ cunit lcov ];
+    configureFlags = drvAttrs.configureFlags ++ [
+      "--enable-debug"
+    ];
+    installPhase = drvAttrs.installPhase + ''
+      echo "Copying test files"
       cp -ar test $out/test
-    fi
-
-    echo $(find $out -type f -name '*.a*' -delete)
-    find . -executable -type f -name 'bdevperf' -exec install -D "{}" $out/bin \;
-  '';
-
-  separateDebugInfo = !enableDebug;
-  dontStrip = true;
-
+    '';
+  });
 }
