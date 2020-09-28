ae37ba724e0be5896643c2e3df3b8d8dfed67b4f
Tom Marsh <thomas.marsh@mayadata.io>
2020-09-28 10:25:02 +0100

    terraform: Require the libvirt provider
    We need this field so that we work with terraform v0.13

--- terraform/mod/libvirt/main.tf
+++ terraform/mod/libvirt/main.tf
@@ -147,5 +147,12 @@ output "node_list" {
 }
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.6.2"
+    }
+  }
 }
