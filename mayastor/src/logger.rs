969f98aa1a3d71a6471d1b628dbfcf0d9a8dc39b
Colin Jones <colin.jones@mayadata.io>
2020-11-16 08:42:14 +0000

    CAS-495 Include more callsite information in trace events:  - Created custom formatter for default subscriber to include    additional information such as file and line number.  - Only generate ansi escape sequences to display LEVEL in colour    if stdout is attached to a terminal.

--- mayastor/src/logger.rs
+++ mayastor/src/logger.rs
@@ -1,8 +1,16 @@
-use std::{ffi::CStr, os::raw::c_char};
+use std::{ffi::CStr, fmt::Write, os::raw::c_char, path::Path};
 
-use tracing_log::format_trace;
+use ansi_term::{Colour, Style};
+
+use tracing_core::{event::Event, Metadata};
+use tracing_log::{format_trace, NormalizeEvent};
 use tracing_subscriber::{
-    fmt::{format::FmtSpan, time::FormatTime, Subscriber},
+    fmt::{
+        format::{FmtSpan, FormatEvent, FormatFields},
+        FmtContext,
+        FormattedFields,
+    },
+    registry::LookupSpan,
     EnvFilter,
 };
 
@@ -43,27 +51,213 @@ pub extern "C" fn log_impl(
         return;
     }
 
-    // remove new line characters from the log messages if any
-    let fmt =
+    let arg =
         unsafe { CStr::from_ptr(buf).to_string_lossy().trim_end().to_string() };
     let filename = unsafe { CStr::from_ptr(file).to_str().unwrap() };
 
     format_trace(
         &log::Record::builder()
-            .args(format_args!("{}", fmt))
-            .target(module_path!())
+            .args(format_args!("{}", arg))
+            .level(from_spdk_level(spdk_level))
+            .target("mayastor::spdk")
             .file(Some(filename))
             .line(Some(line))
-            .level(from_spdk_level(spdk_level))
             .build(),
     )
     .unwrap();
 }
 
-struct CustomTime<'a>(&'a str);
-impl FormatTime for CustomTime<'_> {
-    fn format_time(&self, w: &mut dyn std::fmt::Write) -> std::fmt::Result {
-        write!(w, "{}", chrono::Local::now().format(self.0))
+// Custom struct used to format the log/trace LEVEL
+struct FormatLevel<'a> {
+    level: &'a tracing::Level,
+    ansi: bool,
+}
+
+impl<'a> FormatLevel<'a> {
+    fn new(level: &'a tracing::Level, ansi: bool) -> Self {
+        Self {
+            level,
+            ansi,
+        }
+    }
+}
+
+// Display trace LEVEL.
+impl std::fmt::Display for FormatLevel<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        const TRACE: &str = "TRACE";
+        const DEBUG: &str = "DEBUG";
+        const INFO: &str = " INFO";
+        const WARN: &str = " WARN";
+        const ERROR: &str = "ERROR";
+
+        if self.ansi {
+            match *self.level {
+                tracing::Level::TRACE => {
+                    write!(f, "{}", Colour::Purple.paint(TRACE))
+                }
+                tracing::Level::DEBUG => {
+                    write!(f, "{}", Colour::Blue.paint(DEBUG))
+                }
+                tracing::Level::INFO => {
+                    write!(f, "{}", Colour::Green.paint(INFO))
+                }
+                tracing::Level::WARN => {
+                    write!(f, "{}", Colour::Yellow.paint(WARN))
+                }
+                tracing::Level::ERROR => {
+                    write!(f, "{}", Colour::Red.paint(ERROR))
+                }
+            }
+        } else {
+            match *self.level {
+                tracing::Level::TRACE => f.pad(TRACE),
+                tracing::Level::DEBUG => f.pad(DEBUG),
+                tracing::Level::INFO => f.pad(INFO),
+                tracing::Level::WARN => f.pad(WARN),
+                tracing::Level::ERROR => f.pad(ERROR),
+            }
+        }
+    }
+}
+
+// Custom struct used to format trace context (span) information
+struct CustomContext<'a, S, N>
+where
+    S: tracing_core::subscriber::Subscriber + for<'s> LookupSpan<'s>,
+    N: for<'w> FormatFields<'w> + 'static,
+{
+    context: &'a FmtContext<'a, S, N>,
+    span: Option<&'a tracing_core::span::Id>,
+    ansi: bool,
+}
+
+impl<'a, S, N: 'a> CustomContext<'a, S, N>
+where
+    S: tracing_core::subscriber::Subscriber + for<'s> LookupSpan<'s>,
+    N: for<'w> FormatFields<'w> + 'static,
+{
+    fn new(
+        context: &'a FmtContext<'a, S, N>,
+        span: Option<&'a tracing_core::span::Id>,
+        ansi: bool,
+    ) -> Self {
+        Self {
+            context,
+            span,
+            ansi,
+        }
+    }
+}
+
+// Display trace context (span) information
+impl<'a, S, N> std::fmt::Display for CustomContext<'a, S, N>
+where
+    S: tracing_core::subscriber::Subscriber + for<'s> LookupSpan<'s>,
+    N: for<'w> FormatFields<'w> + 'static,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let bold = if self.ansi {
+            Style::new().bold()
+        } else {
+            Style::new()
+        };
+
+        let scope = self
+            .span
+            .and_then(|ref id| self.context.span(id))
+            .or_else(|| self.context.lookup_current())
+            .into_iter()
+            .flat_map(|span| span.from_root().chain(std::iter::once(span)));
+
+        for span in scope {
+            write!(f, "{}", bold.paint(span.metadata().name()))?;
+
+            let extensions = span.extensions();
+
+            let fields = &extensions
+                .get::<FormattedFields<N>>()
+                .expect("unable to find FormattedFields in extensions");
+
+            if !fields.is_empty() {
+                write!(f, "{}{}{}", bold.paint("{"), fields, bold.paint("}"))?;
+            }
+
+            f.write_char(' ')?;
+        }
+
+        Ok(())
+    }
+}
+
+fn basename(path: &str) -> &str {
+    Path::new(path).file_name().unwrap().to_str().unwrap()
+}
+
+// Custom struct used to format a callsite location (filename and line number)
+struct Location<'a> {
+    meta: &'a Metadata<'a>,
+}
+
+impl<'a> Location<'a> {
+    fn new(meta: &'a Metadata<'a>) -> Self {
+        Self {
+            meta,
+        }
+    }
+}
+
+// Display callsite location (filename and line number) from metadata
+impl std::fmt::Display for Location<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(file) = self.meta.file() {
+            if let Some(line) = self.meta.line() {
+                write!(f, "({}:{}) ", basename(file), line)?;
+            }
+        }
+        Ok(())
+    }
+}
+
+// Custom struct used to format trace events.
+struct CustomFormat {
+    ansi: bool,
+}
+
+// Format a trace event.
+impl<S, N> FormatEvent<S, N> for CustomFormat
+where
+    S: tracing_core::subscriber::Subscriber + for<'s> LookupSpan<'s>,
+    N: for<'w> FormatFields<'w> + 'static,
+{
+    fn format_event(
+        &self,
+        context: &FmtContext<'_, S, N>,
+        writer: &mut dyn std::fmt::Write,
+        event: &Event<'_>,
+    ) -> std::fmt::Result {
+        let normalized = event.normalized_metadata();
+        let meta = normalized.as_ref().unwrap_or_else(|| event.metadata());
+
+        write!(
+            writer,
+            "[{} {} {}] ",
+            chrono::Local::now().format("%FT%T%.9f%Z"),
+            FormatLevel::new(meta.level(), self.ansi),
+            meta.target()
+        )?;
+
+        write!(
+            writer,
+            "{}",
+            CustomContext::new(context, event.parent(), self.ansi)
+        )?;
+
+        write!(writer, "{}", Location::new(&meta))?;
+
+        context.format_fields(writer, event)?;
+
+        writeln!(writer)
     }
 }
 
@@ -74,15 +268,19 @@ impl FormatTime for CustomTime<'_> {
 /// We might want to suppress certain messages, as some of them are redundant,
 /// in particular, the NOTICE messages as such, they are mapped to debug.
 pub fn init(level: &str) {
-    let builder = Subscriber::builder()
-        .with_timer(CustomTime("%FT%T%.9f%Z"))
-        .with_span_events(FmtSpan::FULL);
-
-    let subscriber = if let Ok(filter) = EnvFilter::try_from_default_env() {
-        builder.with_env_filter(filter).finish()
-    } else {
-        builder.with_env_filter(level).finish()
+    let format = CustomFormat {
+        ansi: atty::is(atty::Stream::Stdout),
     };
+
+    let builder = tracing_subscriber::fmt::Subscriber::builder()
+        .with_span_events(FmtSpan::FULL)
+        .event_format(format);
+
+    let subscriber = match EnvFilter::try_from_default_env() {
+        Ok(filter) => builder.with_env_filter(filter).finish(),
+        Err(_) => builder.with_env_filter(level).finish(),
+    };
+
     tracing::subscriber::set_global_default(subscriber)
         .expect("failed to set default subscriber");
 }
