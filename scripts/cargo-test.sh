1da52b88f7e99fe198d9778337feba6a9a66e0f2
Antonin Kral <a.kral@bobek.cz>
2020-11-20 14:58:40 +0100

    Remove all containers after premature end of the tests
    This is considered a workaround (aka ugly hack) for CAS-524. As it
    kills all containers, it doesn't play nicely with other tests being
    executed on the same machine.
    
    Also explicitly switching to `bash` as `sh` doesn't implement
    `pipefail`.

--- scripts/cargo-test.sh
+++ scripts/cargo-test.sh
@@ -1,4 +1,14 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+cleanup_handler() {
+  for c in $(docker ps -a --format '{{.ID}}') ; do
+    docker kill "$c" || true
+    docker rm "$c" || true
+  done
+}
+
+trap cleanup_handler ERR INT QUIT TERM HUP
+
 set -euxo pipefail
 export PATH=$PATH:${HOME}/.cargo/bin
 ( cd jsonrpc && cargo test )
