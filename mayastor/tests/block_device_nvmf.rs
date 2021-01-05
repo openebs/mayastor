ca988626703ca70cdbf8fec931b12aae380ec7ba
Mikhail Tcymbaliuk <mtzaurus@gmail.com>
2021-01-05 22:33:34 +0100

    Vectored write support added to device handle + BDD tests.

--- mayastor/tests/block_device_nvmf.rs
+++ mayastor/tests/block_device_nvmf.rs
@@ -6,9 +6,15 @@ use mayastor::{
     core::{DmaBuf, MayastorCliArgs},
 };
 
-use std::{slice, str, sync::Arc};
-
-use core::sync::atomic::AtomicPtr;
+use std::{
+    alloc::Layout,
+    slice,
+    str,
+    sync::{
+        atomic::{AtomicBool, AtomicPtr, Ordering},
+        Arc,
+    },
+};
 
 use spdk_sys::{self, iovec};
 
@@ -17,6 +23,7 @@ use common::compose::MayastorTest;
 use uuid::Uuid;
 
 static MAYASTOR: OnceCell<MayastorTest> = OnceCell::new();
+static INVOCATION_FLAG: AtomicBool = AtomicBool::new(false);
 
 const MAYASTOR_CTRLR_TITLE: &str = "Mayastor NVMe controler";
 //const MAYASTOR_NQN_PREFIX: &str = "nqn.2019-05.io.openebs:";
@@ -28,6 +35,7 @@ fn get_ms() -> &'static MayastorTest<'static> {
 }
 
 async fn launch_instance() -> String {
+    // TODO: Spawn MayaStor instance properly.
     return "nvmf://172.16.175.130:8420/nqn.2019-05.io.openebs:disk0"
         .to_string();
 }
@@ -46,8 +54,6 @@ async fn nvmf_device_create_destroy() {
         assert_eq!(bdev.driver_name(), "nvme");
         assert_eq!(bdev.device_name(), name1);
 
-        println!("-> block_len: {}", bdev.block_len());
-        println!("-> num_blocks: {}", bdev.num_blocks());
         assert_ne!(bdev.block_len(), 0);
         assert_ne!(bdev.num_blocks(), 0);
         assert_ne!(bdev.size_in_bytes(), 0);
@@ -107,6 +113,23 @@ fn create_io_buffer(alignment: u64, size: u64, pattern: u8) -> DmaBuf {
     }
 
     buf
+}
+
+fn clear_callback_invocation_flag() {
+    INVOCATION_FLAG.store(false, Ordering::Relaxed);
+}
+
+fn flag_callback_invocation() {
+    assert_eq!(
+        INVOCATION_FLAG.compare_exchange(
+            false,
+            true,
+            Ordering::Acquire,
+            Ordering::Relaxed
+        ),
+        Ok(false),
+        "Callback is called more than once"
+    );
 }
 
 #[tokio::test]
@@ -170,7 +193,7 @@ async fn nvmf_device_read_write_at() {
 }
 
 #[tokio::test]
-async fn nvmf_device_read_write_test() {
+async fn nvmf_device_readv_test() {
     const BUF_SIZE: u64 = 32768;
 
     let ms = get_ms();
@@ -186,7 +209,8 @@ async fn nvmf_device_read_write_test() {
 
     // Read completion callback.
     fn read_completion_callback(success: bool, ctx: *const c_void) {
-        println!("readv_blocks() completed !");
+        // Make sure callback is invoked only once.
+        flag_callback_invocation();
 
         assert!(success, "readv_blocks() failed");
         // Make sure we were passed tha same pattern string as requested.
@@ -200,6 +224,9 @@ async fn nvmf_device_read_write_test() {
 
         assert_eq!(s, MAYASTOR_CTRLR_TITLE);
     }
+
+    // Clear callback invocation flag.
+    clear_callback_invocation_flag();
 
     let buf_ptr = ms
         .spawn(async move {
@@ -243,7 +270,7 @@ async fn nvmf_device_read_write_test() {
         .await;
 
     // Sleep for a few seconds to let I/O operation complete.
-    println!("Sleeping for 3 secs to let I/O operation complete");
+    println!("Sleeping for 2 secs to let I/O operation complete");
     tokio::time::delay_for(std::time::Duration::from_secs(3)).await;
     println!("Awakened.");
 
@@ -254,7 +281,405 @@ async fn nvmf_device_read_write_test() {
     check_buf_pattern(unsafe { &((*b).dma_buf) }, 0);
 
     url = Arc::clone(&u);
-    ms.send(async move {
+    ms.spawn(async move {
         device_destroy(&(*url)).await.unwrap();
-    });
+    })
+    .await;
+}
+
+#[tokio::test]
+async fn nvmf_device_writev_test() {
+    const BUF_SIZE: u64 = 128 * 1024;
+    const OP_OFFSET: u64 = 4 * 1024 * 1024;
+
+    let ms = get_ms();
+    let u = Arc::new(launch_instance().await);
+    let mut url = Arc::clone(&u);
+
+    // Read completion callback.
+    fn write_completion_callback(success: bool, ctx: *const c_void) {
+        // Make sure callback is invoked only once.
+        flag_callback_invocation();
+
+        assert!(success, "writev_blocks() failed");
+        // Make sure we were passed tha same pattern string as requested.
+        let s = unsafe {
+            let slice = slice::from_raw_parts(
+                ctx as *const u8,
+                MAYASTOR_CTRLR_TITLE.len(),
+            );
+            str::from_utf8(slice).unwrap()
+        };
+
+        assert_eq!(s, MAYASTOR_CTRLR_TITLE);
+    }
+
+    // Placeholder structure to let all the fields outlive API invocations.
+    struct IoCtx {
+        iov: iovec,
+        dma_buf: DmaBuf,
+        device_name: String,
+    }
+
+    // Clear callback invocation flag.
+    clear_callback_invocation_flag();
+
+    let ctx = ms
+        .spawn(async move {
+            let name = device_create(&(*url)).await.unwrap();
+            let descr = device_open(&name, false).unwrap();
+            let handle = descr.into_handle().unwrap();
+            let device = handle.get_device();
+
+            let guard_buf =
+                create_io_buffer(device.alignment(), BUF_SIZE, GUARD_PATTERN);
+
+            // First, write 2 guard buffers before and after target I/O
+            // location.
+            let mut r = handle.write_at(OP_OFFSET, &guard_buf).await.unwrap();
+            assert_eq!(r, BUF_SIZE, "The amount of data written mismatches");
+            r = handle
+                .write_at(OP_OFFSET + 2 * BUF_SIZE, &guard_buf)
+                .await
+                .unwrap();
+            assert_eq!(r, BUF_SIZE, "The amount of data written mismatches");
+
+            let mut ctx = IoCtx {
+                iov: iovec::default(),
+                device_name: name,
+                dma_buf: create_io_buffer(
+                    device.alignment(),
+                    BUF_SIZE,
+                    IO_PATTERN,
+                ),
+            };
+
+            ctx.iov.iov_base = *ctx.dma_buf;
+            ctx.iov.iov_len = BUF_SIZE;
+
+            // Write data buffer between guard buffers to catch writes outside
+            // the range.
+            handle
+                .writev_blocks(
+                    &mut ctx.iov,
+                    1,
+                    (OP_OFFSET + BUF_SIZE) / device.block_len(),
+                    BUF_SIZE / device.block_len(),
+                    write_completion_callback,
+                    // Use a predefined string to check that we receive the
+                    // same context pointer as we pass upon
+                    // invocation. For this call we don't need any
+                    // specific, operation-related context.
+                    MAYASTOR_CTRLR_TITLE.as_ptr() as *const c_void,
+                )
+                .unwrap();
+
+            AtomicPtr::new(Box::into_raw(Box::new(ctx)))
+        })
+        .await;
+
+    // Sleep for a few seconds to let I/O operation complete.
+    println!("Sleeping for 2 secs to let I/O operation complete");
+    tokio::time::delay_for(std::time::Duration::from_secs(3)).await;
+    println!("Awakened.");
+
+    // Read data just written and check that no boundaries were crossed.
+    url = Arc::clone(&u);
+    ms.spawn(async move {
+        let ctx = unsafe { Box::<IoCtx>::from_raw(ctx.into_inner()) };
+        let device = device_lookup(&ctx.device_name).unwrap();
+        let handle = device.open(true).unwrap().into_handle().unwrap();
+
+        // Check the first guard buffer.
+        let g1 = DmaBuf::new(BUF_SIZE, device.alignment()).unwrap();
+        let mut r = handle.read_at(OP_OFFSET, &g1).await.unwrap();
+        assert_eq!(r, BUF_SIZE, "The amount of data read mismatches");
+        check_buf_pattern(&g1, GUARD_PATTERN);
+
+        // Check the second guard buffer.
+        let g2 = DmaBuf::new(BUF_SIZE, device.alignment()).unwrap();
+        r = handle.read_at(OP_OFFSET + 2 * BUF_SIZE, &g2).await.unwrap();
+        assert_eq!(r, BUF_SIZE, "The amount of data read mismatches");
+        check_buf_pattern(&g2, GUARD_PATTERN);
+
+        // Check the data region between guard buffers.
+        let dbuf = DmaBuf::new(BUF_SIZE, device.alignment()).unwrap();
+        r = handle.read_at(OP_OFFSET + BUF_SIZE, &dbuf).await.unwrap();
+        assert_eq!(r, BUF_SIZE, "The amount of data read mismatches");
+        check_buf_pattern(&dbuf, IO_PATTERN);
+
+        device_destroy(&(*url)).await.unwrap();
+    })
+    .await;
+}
+
+#[tokio::test]
+async fn nvmf_device_readv_iovs_test() {
+    const OP_OFFSET: u64 = 6 * 1024 * 1024;
+    const IOVCNT: usize = 5;
+    const IOVSIZES: [u64; IOVCNT] = [
+        // Sizes of I/O vectors, in kilobytes.
+        512 * 1024,
+        128 * 1024,
+        16 * 1024,
+        256 * 1024,
+        128 * 1024,
+    ];
+    let iosize = IOVSIZES.iter().sum();
+
+    let ms = get_ms();
+    let u = Arc::new(launch_instance().await);
+    let mut url = Arc::clone(&u);
+
+    // Read completion callback.
+    fn read_completion_callback(success: bool, ctx: *const c_void) {
+        // Make sure callback is invoked only once.
+        flag_callback_invocation();
+
+        assert!(success, "readv_blocks() failed");
+        // Make sure we were passed tha same pattern string as requested.
+        let s = unsafe {
+            let slice = slice::from_raw_parts(
+                ctx as *const u8,
+                MAYASTOR_CTRLR_TITLE.len(),
+            );
+            str::from_utf8(slice).unwrap()
+        };
+
+        assert_eq!(s, MAYASTOR_CTRLR_TITLE);
+    }
+
+    // Placeholder structure to let all the fields outlive API invocations.
+    struct IoCtx {
+        iovs: *mut iovec,
+        buffers: Vec<DmaBuf>,
+    }
+
+    // Clear callback invocation flag.
+    clear_callback_invocation_flag();
+
+    let io_ctx = ms
+        .spawn(async move {
+            let device_name = device_create(&(*url)).await.unwrap();
+            let descr = device_open(&device_name, false).unwrap();
+            let handle = descr.into_handle().unwrap();
+            let device = handle.get_device();
+
+            let mut buffers = Vec::<DmaBuf>::with_capacity(IOVCNT);
+
+            // Allocate phsycally continous memory for storing raw I/O vectors.
+            let l = Layout::array::<iovec>(IOVCNT).unwrap();
+            let iovs = unsafe { std::alloc::alloc(l) } as *mut iovec;
+
+            for (i, s) in IOVSIZES.iter().enumerate().take(IOVCNT) {
+                let mut iov = iovec::default();
+                let buf =
+                    create_io_buffer(device.alignment(), *s, GUARD_PATTERN);
+
+                iov.iov_base = *buf;
+                iov.iov_len = buf.len();
+
+                buffers.push(buf);
+                unsafe { *iovs.add(i) = iov };
+            }
+
+            let io_ctx = IoCtx {
+                iovs,
+                buffers,
+            };
+
+            // First, write data pattern of required size.
+            let data_buf =
+                create_io_buffer(device.alignment(), iosize, IO_PATTERN);
+            let r = handle.write_at(OP_OFFSET, &data_buf).await.unwrap();
+            assert_eq!(r, iosize, "The amount of data written mismatches");
+
+            // Initiate a read operation into the I/O vectors.
+            handle
+                .readv_blocks(
+                    io_ctx.iovs,
+                    IOVCNT as i32,
+                    OP_OFFSET / device.block_len(),
+                    iosize / device.block_len(),
+                    read_completion_callback,
+                    // Use a predefined string to check that we receive the
+                    // same context pointer as we pass upon
+                    // invocation. For this call we don't need any
+                    // specific, operation-related context.
+                    MAYASTOR_CTRLR_TITLE.as_ptr() as *const c_void,
+                )
+                .unwrap();
+
+            AtomicPtr::new(Box::into_raw(Box::new(io_ctx)))
+        })
+        .await;
+
+    // Sleep for a few seconds to let I/O operation complete.
+    println!("Sleeping for 2 secs to let I/O operation complete");
+    tokio::time::delay_for(std::time::Duration::from_secs(3)).await;
+    println!("Awakened.");
+
+    url = Arc::clone(&u);
+    ms.spawn(async move {
+        let ctx = unsafe { Box::<IoCtx>::from_raw(io_ctx.into_inner()) };
+
+        for b in &ctx.buffers {
+            check_buf_pattern(b, IO_PATTERN);
+        }
+
+        device_destroy(&(*url)).await.unwrap();
+    })
+    .await;
+}
+
+#[tokio::test]
+async fn nvmf_device_writev_iovs_test() {
+    const GUARD_SIZE: u64 = 32 * 1024;
+    const OP_OFFSET: u64 = 10 * 1024 * 1024;
+    const IOVCNT: usize = 6;
+    const IOVSIZES: [u64; IOVCNT] = [
+        // Sizes of I/O vectors, in kilobytes.
+        512 * 1024,
+        256 * 1024,
+        512 * 1024,
+        8 * 1024,
+        128 * 1024,
+        64 * 1024,
+    ];
+    let iosize = IOVSIZES.iter().sum();
+
+    let ms = get_ms();
+    let u = Arc::new(launch_instance().await);
+    let mut url = Arc::clone(&u);
+
+    // Clear callback invocation flag.
+    clear_callback_invocation_flag();
+
+    // Read completion callback.
+    fn read_completion_callback(success: bool, ctx: *const c_void) {
+        // Make sure callback is invoked only once.
+        flag_callback_invocation();
+
+        assert!(success, "readv_blocks() failed");
+        // Make sure we were passed tha same pattern string as requested.
+        let s = unsafe {
+            let slice = slice::from_raw_parts(
+                ctx as *const u8,
+                MAYASTOR_CTRLR_TITLE.len(),
+            );
+            str::from_utf8(slice).unwrap()
+        };
+
+        assert_eq!(s, MAYASTOR_CTRLR_TITLE);
+    }
+
+    // Placeholder structure to let all the fields outlive API invocations.
+    struct IoCtx {
+        iovs: *mut iovec,
+        buffers: Vec<DmaBuf>,
+        device_name: String,
+    }
+
+    let io_ctx = ms
+        .spawn(async move {
+            let device_name = device_create(&(*url)).await.unwrap();
+            let descr = device_open(&device_name, false).unwrap();
+            let handle = descr.into_handle().unwrap();
+            let device = handle.get_device();
+
+            let mut buffers = Vec::<DmaBuf>::with_capacity(IOVCNT);
+
+            // Allocate phsycally continous memory for storing raw I/O vectors.
+            let l = Layout::array::<iovec>(IOVCNT).unwrap();
+            let iovs = unsafe { std::alloc::alloc(l) } as *mut iovec;
+
+            for (i, s) in IOVSIZES.iter().enumerate().take(IOVCNT) {
+                let mut iov = iovec::default();
+                let buf = create_io_buffer(device.alignment(), *s, IO_PATTERN);
+
+                iov.iov_base = *buf;
+                iov.iov_len = buf.len();
+
+                buffers.push(buf);
+                unsafe { *iovs.add(i) = iov };
+            }
+
+            let io_ctx = IoCtx {
+                iovs,
+                buffers,
+                device_name,
+            };
+
+            // First, write 2 guard buffers before and after target I/O
+            // location.
+            let guard_buf =
+                create_io_buffer(device.alignment(), GUARD_SIZE, GUARD_PATTERN);
+            let mut r = handle
+                .write_at(OP_OFFSET - GUARD_SIZE, &guard_buf)
+                .await
+                .unwrap();
+            assert_eq!(r, GUARD_SIZE, "The amount of data written mismatches");
+            r = handle
+                .write_at(OP_OFFSET + iosize, &guard_buf)
+                .await
+                .unwrap();
+            assert_eq!(r, GUARD_SIZE, "The amount of data written mismatches");
+
+            // Initiate a write operation into the I/O vectors.
+            handle
+                .writev_blocks(
+                    io_ctx.iovs,
+                    IOVCNT as i32,
+                    OP_OFFSET / device.block_len(),
+                    iosize / device.block_len(),
+                    read_completion_callback,
+                    // Use a predefined string to check that we receive the
+                    // same context pointer as we pass upon
+                    // invocation. For this call we don't need any
+                    // specific, operation-related context.
+                    MAYASTOR_CTRLR_TITLE.as_ptr() as *const c_void,
+                )
+                .unwrap();
+
+            AtomicPtr::new(Box::into_raw(Box::new(io_ctx)))
+        })
+        .await;
+
+    // Sleep for a few seconds to let I/O operation complete.
+    println!("Sleeping for 2 secs to let I/O operation complete");
+    tokio::time::delay_for(std::time::Duration::from_secs(3)).await;
+    println!("Awakened.");
+
+    url = Arc::clone(&u);
+    ms.spawn(async move {
+        let ctx = unsafe { Box::<IoCtx>::from_raw(io_ctx.into_inner()) };
+        let device = device_lookup(&ctx.device_name).unwrap();
+        let handle = device.open(true).unwrap().into_handle().unwrap();
+
+        // Make sure buffers content didn't change.
+        for b in &ctx.buffers {
+            check_buf_pattern(b, IO_PATTERN);
+        }
+
+        // Check the first guard buffer.
+        let g1 = DmaBuf::new(GUARD_SIZE, device.alignment()).unwrap();
+        let mut r = handle.read_at(OP_OFFSET - GUARD_SIZE, &g1).await.unwrap();
+        assert_eq!(r, GUARD_SIZE, "The amount of data read mismatches");
+        check_buf_pattern(&g1, GUARD_PATTERN);
+
+        // Check the second guard buffer.
+        let g2 = DmaBuf::new(GUARD_SIZE, device.alignment()).unwrap();
+        r = handle.read_at(OP_OFFSET + iosize, &g2).await.unwrap();
+        assert_eq!(r, GUARD_SIZE, "The amount of data read mismatches");
+        check_buf_pattern(&g2, GUARD_PATTERN);
+
+        // Check the data region between guard buffers.
+        let dbuf = DmaBuf::new(iosize, device.alignment()).unwrap();
+        r = handle.read_at(OP_OFFSET, &dbuf).await.unwrap();
+        assert_eq!(r, iosize, "The amount of data read mismatches");
+        check_buf_pattern(&dbuf, IO_PATTERN);
+
+        device_destroy(&(*url)).await.unwrap();
+    })
+    .await;
 }
