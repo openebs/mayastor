f004416fd96a1c827cca79b82ea4d4b7490c856b
Jeffry Molanus <jeffry.molanus@gmail.com>
2021-01-08 14:47:13 +0100

    nvmx: abstract list of nvmx devices
    Implement a struct that holds the list of nvmx devices.

--- mayastor/src/bdev/dev/nvmx/mod.rs
+++ mayastor/src/bdev/dev/nvmx/mod.rs
@@ -13,56 +13,96 @@ pub use handle::NvmeDeviceHandle;
 pub use namespace::NvmeNamespace;
 pub(crate) use uri::NvmfDeviceTemplate;
 
-use crate::{core::CoreError, subsys::{Config, NvmeBdevOpts}};
+use crate::{
+    core::CoreError,
+    subsys::{Config, NvmeBdevOpts},
+};
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex, RwLock},
+    fmt::Display,
+    sync::{Arc, Mutex, RwLock, RwLockReadGuard, RwLockWriteGuard},
 };
 
-// TODO make this a struct
-lazy_static! {
-    pub static ref NVME_CONTROLLERS: RwLock<HashMap<String, Arc<Mutex<NvmeController>>>> =
-        RwLock::new(HashMap::<String, Arc<Mutex<NvmeController>>>::new());
+use once_cell::sync::Lazy;
+
+#[derive(Debug)]
+pub(crate) struct NVMeCtlrList {
+    entries: RwLock<HashMap<String, Arc<Mutex<NvmeController>>>>,
 }
 
-pub fn nvme_controller_lookup(
-    name: &str,
-) -> Option<Arc<Mutex<NvmeController>>> {
-    let controllers = NVME_CONTROLLERS.read().unwrap();
-    if let Some(instance) = controllers.get(name) {
-        Some(Arc::clone(instance))
-    } else {
-        info!("NVMe controller {} not found", name);
-        None
+impl NVMeCtlrList {
+    fn write_lock(
+        &self,
+    ) -> RwLockWriteGuard<HashMap<String, Arc<Mutex<NvmeController>>>> {
+        self.entries.write().expect("rwlock poisoned")
+    }
+
+    fn read_lock(
+        &self,
+    ) -> RwLockReadGuard<HashMap<String, Arc<Mutex<NvmeController>>>> {
+        self.entries.read().expect("rwlock poisoned")
+    }
+
+    /// lookup a NVMe controller
+    pub fn lookup_by_name<T: Into<String>>(
+        &self,
+        name: T,
+    ) -> Option<Arc<Mutex<NvmeController>>> {
+        let entries = self.read_lock();
+        entries.get(&name.into()).map(|e| Arc::clone(e))
+    }
+
+    /// remove a NVMe controller from the list, when the last reference to the
+    /// controller is dropped, the controller will be freed.
+
+    pub fn remove_by_name<T: Into<String> + Display>(
+        &self,
+        name: T,
+    ) -> Result<String, CoreError> {
+        let mut entries = self.write_lock();
+
+        if !entries.contains_key(&name.to_string()) {
+            return Err(CoreError::BdevNotFound {
+                name: name.into(),
+            });
+        }
+
+        // Remove 'controller name -> controller' mapping.
+        let e = entries.remove(&name.to_string()).unwrap();
+        let controller = e.lock().unwrap();
+
+        // Remove 'controller id->controller' mapping. This will remove the last
+        // reference as causes the controller to be dropped.
+        entries.remove(&controller.id().to_string());
+
+        debug!("{}: NVMe controller has been removed from the list", name);
+        Ok(name.into())
+    }
+
+    /// insert a controller into the list using the key, note that different
+    /// keys may refer to the same controller
+    pub fn insert_controller(
+        &self,
+        cid: String,
+        ctl: Arc<Mutex<NvmeController>>,
+    ) {
+        let mut entries = self.write_lock();
+        entries.insert(cid, ctl);
     }
 }
 
-/// NVMe controllers are stored using multiple keys to the same value. This allows
-/// for easy lookup. As a consequence however, both keys must be removed in order
-// for the controller to get dropped.
-pub fn nvme_controller_remove(name: String) -> Result<String, CoreError>{
-
-    debug!("{}: removing NVMe controller", name);
-
-    let mut controllers = NVME_CONTROLLERS.write().unwrap();
-    if !controllers.contains_key(&name) {
-        return Err(CoreError::BdevNotFound {
-            name
-        });
+impl Default for NVMeCtlrList {
+    fn default() -> Self {
+        Self {
+            entries: RwLock::new(
+                HashMap::<String, Arc<Mutex<NvmeController>>>::new(),
+            ),
+        }
     }
-
-    // Remove 'controller name -> controller' mapping.
-    let e = controllers.remove(&name).unwrap();
-    let controller = e.lock().unwrap();
-
-    // Remove 'controller id->controller' mapping. This will remove the last reference as
-    // causes the controller to be dropped.
-    controllers.remove(&controller.id().to_string());
-
-    debug!("{}: NVMe controller has been removed from the list", name);
-    Ok(name.into())
-
 }
+
+static NVME_CONTROLLERS: Lazy<NVMeCtlrList> =
+    Lazy::new(|| NVMeCtlrList::default());
 
 pub fn nvme_bdev_running_config() -> &'static NvmeBdevOpts {
     &Config::get().nvme_bdev_opts
