8a62a42f008c868578688a1d03b13ec3c3b2f259
Tiago Castro <tiago.castro@mayadata.io>
2021-01-19 23:08:49 +0000

    fix(lint): sanitize PR messages
    Sanitize PR title and body when doing the lint pull request stage.
    Also explicitly set type-enum values so we add "example" to the list.

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,7 @@ module.exports = {
   rules: {
     "header-max-length": async () => [2, "always", 50],
     "body-max-line-length": async () => [2, "always", 72],
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'example']],
   },
-  defaultIgnores: false,    
+  defaultIgnores: false,
 }
