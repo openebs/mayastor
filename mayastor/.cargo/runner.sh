460ba7ee289c619e7a7c0ffe2eb0a1a64590ab69
Ana Hobden <operator@hoverbear.org>
2021-01-20 10:15:41 -0800

    chore: handle sudoless root
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- mayastor/.cargo/runner.sh
+++ mayastor/.cargo/runner.sh
@@ -1,15 +1,20 @@
 #! /usr/bin/env bash
-set -x
 
 # Grab the arguments passed to the runner.
 ARGS="${@}"
+
+if [[ $EUID -ne 0 ]]; then
+  MAYBE_SUDO='sudo -E'
+else
+  MAYBE_SUDO=''
+fi
 
 # Elevate to sudo so we can set some capabilities via `capsh`, then execute the args with the required capabilities:
 #
 # * Set `cap_setpcap` to be able to set [ambient capabilities](https://lwn.net/Articles/636533/) which can be inherited
 # by children.
 # * Set `cap_sys_admin,cap_ipc_lock,cap_sys_nice` as they are required by `mayastor`.
-sudo -E capsh \
+${MAYBE_SUDO} capsh \
   --caps="cap_setpcap+iep cap_sys_admin,cap_ipc_lock,cap_sys_nice+iep" \
   --addamb=cap_sys_admin --addamb=cap_ipc_lock --addamb=cap_sys_nice \
   -- -c "${ARGS}"
