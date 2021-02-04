05e1af4ea7bc164855cbaa11bb2233e55bb0c96d
Ana Hobden <operator@hoverbear.org>
2021-02-04 14:05:44 -0800

    fix: correct safety of maya_log
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

--- spdk-sys/logwrapper.c
+++ spdk-sys/logwrapper.c
@@ -5,7 +5,7 @@ maya_log(int level, const char *file, const int line, const char *func,
     const char *format, va_list args)
 {
     char buf[1024] = {0};
-    int n_written = vsnprintf(buf, sizeof(buf), format, args);
-    logfn(level, file, line, func, &buf[0], n_written);
+    unsigned int would_have_written = vsnprintf(buf, sizeof(buf), format, args);
+    logfn(level, file, line, func, &buf[0], ((would_have_written > sizeof(buf)) ? sizeof(buf) : would_have_written));
 }
 
