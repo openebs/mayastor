1f8cac0e490d7b7e67ddec800e07d6f57e48f77c
Mikhail Tcymbaliuk <mtzaurus@gmail.com>
2021-03-12 19:14:44 +0100

    feat(nvmx): abstract block devices for nexus children
    Nexus now treats all its replica devices as abstract block devices,
    which allows clear separation, and uses abstract block device I/O
    handles for serving child I/O.
    
    In order to prevent start of controller resets in response to I/O errors
    (mostly connection failures), maximum number of controller resets
    introduced along with reset cool down interval.
    
    Per-channel I/O operation accounting is introduced to have better I/O
    observability/statistics.
    
    Implements CAS-781

--- mayastor/src/bdev/nexus/nexus_module.rs
+++ mayastor/src/bdev/nexus/nexus_module.rs
@@ -5,7 +5,6 @@ use serde_json::json;
 
 use spdk_sys::{
     spdk_bdev_module,
-    spdk_bdev_module_examine_done,
     spdk_bdev_module_list_add,
     spdk_get_thread,
     spdk_json_write_ctx,
@@ -13,10 +12,7 @@ use spdk_sys::{
 };
 
 use crate::{
-    bdev::nexus::{
-        nexus_bdev::{Nexus, NexusState},
-        nexus_io::NioCtx,
-    },
+    bdev::nexus::{nexus_bdev::Nexus, nexus_io::NioCtx},
     core::{Bdev, Reactor},
 };
 
@@ -58,7 +54,7 @@ impl NexusModule {
         module.module_init = Some(Self::nexus_mod_init);
         module.module_fini = Some(Self::nexus_mod_fini);
         module.get_ctx_size = Some(Self::nexus_ctx_size);
-        module.examine_config = Some(Self::examine);
+        module.examine_config = None;
         module.examine_disk = None;
         module.config_json = Some(Self::config_json);
         NexusModule(Box::into_raw(module))
@@ -116,40 +112,6 @@ impl NexusModule {
         Self::get_instances().clear();
     }
 
-    /// called for each bdev that gets added to the system
-    extern "C" fn examine(new_device: *mut spdk_sys::spdk_bdev) {
-        let name = Bdev::from(new_device).name();
-        let instances = Self::get_instances();
-
-        instances
-            .iter_mut()
-            .filter(|nexus| *nexus.state.lock().unwrap() == NexusState::Init)
-            .any(|nexus| {
-                if nexus.examine_child(&name) {
-                    info!(
-                        "child {} for nexus {} came online",
-                        name, nexus.name
-                    );
-                    // we use block on here as it makes any log message
-                    // show in within proper order. We can also dispatch
-                    // a future and have it executed at the next poll, but
-                    // this is more inline with what the other modules are
-                    // doing as well.
-                    Reactor::block_on(async move {
-                        if nexus.open().await.is_err() {
-                            debug!("nexus {} still not completed", nexus.name);
-                        }
-                    });
-                    return true;
-                }
-                false
-            });
-
-        unsafe {
-            spdk_bdev_module_examine_done(NEXUS_MODULE.0 as *const _ as *mut _)
-        }
-    }
-
     extern "C" fn nexus_ctx_size() -> i32 {
         std::mem::size_of::<NioCtx>() as i32
     }
@@ -164,7 +126,7 @@ impl NexusModule {
             let uris = nexus
                 .children
                 .iter()
-                .map(|c| c.name.clone())
+                .map(|c| c.get_name().to_string())
                 .collect::<Vec<String>>();
 
             let json = json!({
