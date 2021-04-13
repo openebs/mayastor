openebs/mayastor

commit 98f8a06b84eb61a7fcb44e43371a1eab991d06fe

style(lint): lint commit messages
Add pre-commit hook to lint commit messages locally

shell.nix
@@ -69,6 +69,6 @@ mkShell {
     ${pkgs.lib.optionalString (norust) "cowsay ${norust_moth}"}
     ${pkgs.lib.optionalString (norust) "echo 'Hint: use rustup tool.'"}
     ${pkgs.lib.optionalString (norust) "echo"}
-    pre-commit install
+    pre-commit install --hook commit-msg
   '';
 }

.pre-commit-config.yaml
@@ -30,3 +30,11 @@ repos:
         pass_filenames: true
         types: [file, javascript]
         language: system
+    -   id: commit-lint
+        name: Commit Lint
+        description: Runs commitlint against the commit message.
+        language: system
+        entry: bash -c "cat $1 | npx commitlint"
+        args: [$1]
+        stages: [commit-msg]
+

commit aece726cc1f33030db9fa983f25c4cbc2d326e29

fix: restore pre-commit hook
Restore the pre-commit hook installation that was inadvertently
removed.

Fixes: 98f8a06b  ("style(lint): lint commit messages")

shell.nix
@@ -73,6 +73,7 @@ mkShell {
     ${pkgs.lib.optionalString (norust) "cowsay ${norust_moth}"}
     ${pkgs.lib.optionalString (norust) "echo 'Hint: use rustup tool.'"}
     ${pkgs.lib.optionalString (norust) "echo"}
+    pre-commit install
     pre-commit install --hook commit-msg
   '';
 }