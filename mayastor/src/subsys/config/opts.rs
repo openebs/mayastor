openebs/mayastor

commit 682e802e7db2b9517bda67df540e9e7d7428856c

mayastor: Build against SPDK 20.10
Sync with upstream changes to spdk_nvmf_subsystem,
spdk_nvmf_transport_opts and spdk_iscsi_opts.
Specify public portal group in iscsi_portal_grp_create.
Sync with renamed nvmf_tgt_accept.

Remove libspdk_sock_uring.a before building libspdk.so to remove the
use of io_uring for sockets as this is not supported on kernel 5.4.

Update libspdk to build against the v20.10.x-mayastor branch
and update the spdk-sys readme.

mayastor/src/subsys/config/opts.rs
@@ -186,13 +186,10 @@ impl From<TcpTransportOpts> for spdk_nvmf_transport_opts {
             max_aq_depth: o.max_aq_depth,
             num_shared_buffers: o.num_shared_buf,
             buf_cache_size: o.buf_cache_size,
-            max_srq_depth: o.max_srq_depth,
-            no_srq: o.no_srq,
-            c2h_success: o.ch2_success,
             dif_insert_or_strip: o.dif_insert_or_strip,
-            sock_priority: o.sock_priority,
-            acceptor_backlog: 0,
             abort_timeout_sec: 0,
+            association_timeout: 120000,
+            transport_specific: std::ptr::null(),
         }
     }
 }
@@ -392,6 +389,10 @@ pub struct IscsiTgtOpts {
     error_recovery_level: u32,
     /// todo
     allow_duplicate_isid: bool,
+    /// todo
+    max_large_data_in_per_connection: u32,
+    /// todo
+    max_r2t_per_connection: u32,
 }
 
 impl Default for IscsiTgtOpts {
@@ -415,6 +416,8 @@ impl Default for IscsiTgtOpts {
             immediate_data: true,
             error_recovery_level: 0,
             allow_duplicate_isid: false,
+            max_large_data_in_per_connection: 64,
+            max_r2t_per_connection: 64,
         }
     }
 }
@@ -444,6 +447,8 @@ impl From<&IscsiTgtOpts> for spdk_iscsi_opts {
             ImmediateData: o.immediate_data,
             ErrorRecoveryLevel: o.error_recovery_level,
             AllowDuplicateIsid: o.allow_duplicate_isid,
+            MaxLargeDataInPerConnection: o.max_large_data_in_per_connection,
+            MaxR2TPerConnection: o.max_r2t_per_connection,
         }
     }
 }

mayastor/src/lvs/lvs_pool.rs
@@ -243,11 +243,12 @@ impl Lvs {
     /// Create a pool on base bdev
     pub async fn create(name: &str, bdev: &str) -> Result<Lvs, Error> {
         let pool_name = name.into_cstring();
+        let bdev_name = bdev.into_cstring();
 
         let (sender, receiver) = pair::<ErrnoResult<Lvs>>();
         unsafe {
             vbdev_lvs_create(
-                Bdev::lookup_by_name(bdev).unwrap().as_ptr(),
+                bdev_name.as_ptr(),
                 pool_name.as_ptr(),
                 0,
                 // We used to clear a pool with UNMAP but that takes awfully

mayastor/src/subsys/nvmf/subsystem.rs
@@ -101,7 +101,10 @@ impl Debug for NvmfSubsystem {
                 .field("subnqn", &self.0.as_ref().subnqn.as_str().to_string())
                 .field("sn", &self.0.as_ref().sn.as_str().to_string())
                 .field("mn", &self.0.as_ref().mn.as_str().to_string())
-                .field("allow_any_host", &self.0.as_ref().allow_any_host)
+                .field(
+                    "allow_any_host",
+                    &self.0.as_ref().flags.allow_any_host(),
+                )
                 .field("listeners", &self.listeners_to_vec())
                 .finish()
         }

mayastor/src/subsys/nvmf/target.rs
@@ -7,13 +7,13 @@ use std::{
 use nix::errno::Errno;
 
 use spdk_sys::{
+    nvmf_tgt_accept,
     spdk_env_get_core_count,
     spdk_nvmf_poll_group_destroy,
     spdk_nvmf_subsystem_create,
     spdk_nvmf_subsystem_set_mn,
     spdk_nvmf_target_opts,
     spdk_nvmf_tgt,
-    spdk_nvmf_tgt_accept,
     spdk_nvmf_tgt_create,
     spdk_nvmf_tgt_destroy,
     spdk_nvmf_tgt_listen,
@@ -246,8 +246,7 @@ impl Target {
     }
     /// poll function that the acceptor runs
     extern "C" fn acceptor_poll(tgt: *mut c_void) -> i32 {
-        let tgt = tgt as *mut spdk_nvmf_tgt;
-        unsafe { spdk_nvmf_tgt_accept(tgt) };
+        unsafe { nvmf_tgt_accept(tgt) };
 
         0
     }

mayastor/src/target/iscsi.rs
@@ -347,7 +347,7 @@ fn create_portal_group(
 
     let portal_port = CString::new(port_no.to_string()).unwrap();
     let portal_host = CString::new(address.to_owned()).unwrap();
-    let pg = unsafe { iscsi_portal_grp_create(pg_no) };
+    let pg = unsafe { iscsi_portal_grp_create(pg_no, false) };
     if pg.is_null() {
         return Err(Error::CreatePortalGroup {});
     }

mayastor/src/target/nvmf.rs
@@ -19,6 +19,7 @@ use once_cell::sync::Lazy;
 use snafu::{ResultExt, Snafu};
 
 use spdk_sys::{
+    nvmf_tgt_accept,
     spdk_nvme_transport_id,
     spdk_nvmf_poll_group,
     spdk_nvmf_poll_group_add,
@@ -41,7 +42,6 @@ use spdk_sys::{
     spdk_nvmf_subsystem_stop,
     spdk_nvmf_target_opts,
     spdk_nvmf_tgt,
-    spdk_nvmf_tgt_accept,
     spdk_nvmf_tgt_add_transport,
     spdk_nvmf_tgt_create,
     spdk_nvmf_tgt_destroy,
@@ -500,7 +500,7 @@ impl Target {
     extern "C" fn acceptor_poll(target_ptr: *mut c_void) -> c_int {
         unsafe {
             let target = &mut *(target_ptr as *mut Self);
-            spdk_nvmf_tgt_accept(target.inner);
+            nvmf_tgt_accept(target.inner.cast());
         }
         -1
     }

nix/pkgs/libspdk/default.nix
@@ -19,13 +19,13 @@
 let
   # Derivation attributes for production version of libspdk
   drvAttrs = rec {
-    version = "20.07";
+    version = "20.10";
 
     src = fetchFromGitHub {
       owner = "openebs";
       repo = "spdk";
-      rev = "b09bed5edaca1d827a6432ca602639d43e3e93a0";
-      sha256 = "0y26p4m99gbnf6iz2vbai26msnry7m428g8q3icpg28izmnk00d1";
+      rev = "46b25360887c5d19433f575c7ad14259721abc6f";
+      sha256 = "0cjnpkqx95cgrk9kbm4drrd5piimprz7wsbiahsllm1j2avdzsfs";
       #sha256 = stdenv.lib.fakeSha256;
       fetchSubmodules = true;
     };
@@ -74,6 +74,7 @@ let
     buildPhase = ''
       make -j`nproc`
       find . -type f -name 'libspdk_event_nvmf.a' -delete
+      find . -type f -name 'libspdk_sock_uring.a' -delete
       find . -type f -name 'libspdk_ut_mock.a' -delete
 
       $CC -shared -o libspdk.so \

spdk-sys/build.rs
@@ -84,6 +84,7 @@ fn main() {
         .whitelist_function("^vbdev_.*")
         .whitelist_function("^nvme_cmd_.*")
         .whitelist_function("^nvme_status_.*")
+        .whitelist_function("^nvmf_tgt_accept")
         .blacklist_type("^longfunc")
         .whitelist_var("^NVMF.*")
         .whitelist_var("^SPDK.*")

commit 5f1132bcd48b86b274e6f996980599b1d895d3f0

fix(iscsi): use correct upstream default for MaxR2TPerConnection
Upstream SPDK defaults to 4 for MaxR2TPerConnection, not 64, so
"PDU Pool" was 16X larger than it needs to be.

Fixes: 682e802 ("mayastor: Build against SPDK 20.10")

mayastor/src/subsys/config/opts.rs
@@ -458,7 +458,7 @@ impl Default for IscsiTgtOpts {
             error_recovery_level: 0,
             allow_duplicate_isid: false,
             max_large_data_in_per_connection: 64,
-            max_r2t_per_connection: 64,
+            max_r2t_per_connection: 4,
         }
     }
 }