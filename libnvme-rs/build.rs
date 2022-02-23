fda7304ad478b408677599ef1bf018cdf03c0453
Jonathan Teh <30538043+jonathan-teh@users.noreply.github.com>
2022-02-23 14:27:01 +0000

    feat(libnvme-rs): implement connect and disconnect
    Reuse NvmeTarget, and some NvmeErrors, from nvmeadm and implement
    connect and disconnect to an NVMe-oF target using libnvme.
    
    Switch cargo tests to use libnvme-rs instead of nvmeadm.

---
+++ libnvme-rs/build.rs
@@ -0,0 +1,20 @@
+extern crate bindgen;
+
+use std::{env, path::PathBuf};
+
+fn main() {
+    println!("cargo:rustc-link-lib=nvme");
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .layout_tests(false)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ libnvme-rs/src/nvme_uri.rs
@@ -0,0 +1,292 @@
+use std::{convert::TryFrom, time::Duration};
+
+use url::{ParseError, Url};
+
+use crate::{
+    error::NvmeError,
+    nvme_tree::{
+        NvmeCtrlrIterator,
+        NvmeHostIterator,
+        NvmeNamespaceIterator,
+        NvmeRoot,
+        NvmeSubsystemIterator,
+    },
+};
+
+/// Wrapper for caller-owned C-strings from libnvme
+pub struct NvmeStringWrapper {
+    s: *mut i8,
+}
+
+impl NvmeStringWrapper {
+    pub fn new(s: *mut i8) -> Self {
+        NvmeStringWrapper {
+            s,
+        }
+    }
+    pub fn as_ptr(&self) -> *const i8 {
+        self.s
+    }
+}
+
+impl Drop for NvmeStringWrapper {
+    fn drop(&mut self) {
+        unsafe { libc::free(self.s as *mut _) }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum NvmeTransportType {
+    Tcp,
+}
+
+impl NvmeTransportType {
+    fn to_str(&self) -> &str {
+        match self {
+            NvmeTransportType::Tcp => "tcp",
+        }
+    }
+}
+
+/// Structure representing an NVMe-oF target
+pub struct NvmeTarget {
+    /// Network address
+    traddr: String,
+    /// Transport service ID. For IP transports, a port number
+    trsvcid: u16,
+    /// Name of subsystem to connect to
+    subsysnqn: String,
+    /// Transport type
+    trtype: NvmeTransportType,
+}
+
+impl TryFrom<String> for NvmeTarget {
+    type Error = NvmeError;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        NvmeTarget::try_from(value.as_str())
+    }
+}
+
+impl TryFrom<&str> for NvmeTarget {
+    type Error = NvmeError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let url = Url::parse(value).map_err(|source| NvmeError::UrlError {
+            source,
+        })?;
+
+        let trtype = match url.scheme() {
+            "nvmf" | "nvmf+tcp" => Ok(NvmeTransportType::Tcp),
+            _ => Err(NvmeError::UrlError {
+                source: ParseError::IdnaError,
+            }),
+        }?;
+
+        let traddr = url
+            .host_str()
+            .ok_or(NvmeError::UrlError {
+                source: ParseError::EmptyHost,
+            })?
+            .into();
+
+        let subnqn = match url.path_segments() {
+            None => Err(NvmeError::UrlError {
+                source: ParseError::RelativeUrlWithCannotBeABaseBase,
+            }),
+            Some(s) => {
+                let segments = s.collect::<Vec<&str>>();
+                if segments[0].is_empty() {
+                    Err(NvmeError::UrlError {
+                        source: ParseError::RelativeUrlWithCannotBeABaseBase,
+                    })
+                } else {
+                    Ok(segments[0].to_string())
+                }
+            }
+        }?;
+
+        Ok(Self {
+            trtype,
+            traddr,
+            trsvcid: url.port().unwrap_or(4420),
+            subsysnqn: subnqn,
+        })
+    }
+}
+
+impl NvmeTarget {
+    /// Connect to NVMe target
+    /// Returns Ok on successful connect
+    pub fn connect(&self) -> Result<(), NvmeError> {
+        let r = NvmeRoot::new(unsafe { crate::nvme_scan(std::ptr::null()) });
+        let hostnqn =
+            NvmeStringWrapper::new(unsafe { crate::nvmf_hostnqn_from_file() });
+        let hostid =
+            NvmeStringWrapper::new(unsafe { crate::nvmf_hostid_from_file() });
+
+        let h = unsafe {
+            crate::nvme_lookup_host(
+                r.as_mut_ptr(),
+                hostnqn.as_ptr(),
+                hostid.as_ptr(),
+            )
+        };
+        if h.is_null() {
+            return Err(NvmeError::LookupHostError {
+                rc: -libc::ENOMEM,
+            });
+        }
+
+        let subsysnqn = std::ffi::CString::new(self.subsysnqn.clone()).unwrap();
+        let transport = std::ffi::CString::new(self.trtype.to_str()).unwrap();
+        let traddr = std::ffi::CString::new(self.traddr.clone()).unwrap();
+        let host_traddr = std::ptr::null_mut();
+        let host_iface = std::ptr::null_mut();
+        let trsvcid = std::ffi::CString::new(self.trsvcid.to_string()).unwrap();
+        let nvme_ctrl = unsafe {
+            crate::nvme_create_ctrl(
+                r.as_mut_ptr(),
+                subsysnqn.as_ptr(),
+                transport.as_ptr(),
+                traddr.as_ptr(),
+                host_traddr,
+                host_iface,
+                trsvcid.as_ptr(),
+            )
+        };
+        if nvme_ctrl.is_null() {
+            return Err(NvmeError::CreateCtrlrError {
+                rc: -libc::ENOMEM,
+            });
+        }
+        let cfg = crate::nvme_fabrics_config {
+            host_traddr,
+            host_iface,
+            queue_size: 0,
+            nr_io_queues: 0,
+            reconnect_delay: 0,
+            ctrl_loss_tmo: crate::NVMF_DEF_CTRL_LOSS_TMO as i32,
+            fast_io_fail_tmo: 0,
+            keep_alive_tmo: 0,
+            nr_write_queues: 0,
+            nr_poll_queues: 0,
+            tos: -1,
+            duplicate_connect: false,
+            disable_sqflow: false,
+            hdr_digest: false,
+            data_digest: false,
+            tls: false,
+        };
+        let ret = unsafe { crate::nvmf_add_ctrl(h, nvme_ctrl, &cfg) };
+        if ret != 0 {
+            return Err(NvmeError::AddCtrlrError {
+                rc: ret,
+            });
+        }
+        Ok(())
+    }
+
+    /// List block devices for this target
+    ///
+    /// `retries`: number of times to retry until at least one device is found
+    pub fn block_devices(
+        &self,
+        mut retries: usize,
+    ) -> Result<Vec<String>, NvmeError> {
+        let mut devices = Vec::<String>::new();
+        loop {
+            let r =
+                NvmeRoot::new(unsafe { crate::nvme_scan(std::ptr::null()) });
+            let hostiter = NvmeHostIterator::new(&r);
+            for host in hostiter {
+                let subsysiter = NvmeSubsystemIterator::new(host);
+                for subsys in subsysiter {
+                    let cstr = unsafe {
+                        std::ffi::CStr::from_ptr(crate::nvme_subsystem_get_nqn(
+                            subsys,
+                        ))
+                    };
+                    if cstr.to_str().unwrap() != self.subsysnqn {
+                        continue;
+                    }
+                    let nsiter = NvmeNamespaceIterator::new(subsys);
+                    for ns in nsiter {
+                        devices.push(format!(
+                            "/dev/{}",
+                            unsafe {
+                                std::ffi::CStr::from_ptr(
+                                    crate::nvme_ns_get_name(ns),
+                                )
+                            }
+                            .to_str()
+                            .unwrap()
+                        ));
+                    }
+                }
+            }
+
+            if retries == 0 || !devices.is_empty() {
+                break;
+            }
+            retries -= 1;
+            std::thread::sleep(Duration::from_millis(500));
+        }
+
+        Ok(devices)
+    }
+
+    /// Disconnect from NVMe target
+    ///
+    /// Returns number of controllers disconnected
+    pub fn disconnect(&self) -> Result<usize, NvmeError> {
+        let r = NvmeRoot::new(unsafe { crate::nvme_scan(std::ptr::null()) });
+        let mut i = 0;
+        let hostiter = NvmeHostIterator::new(&r);
+        for host in hostiter {
+            let subsysiter = NvmeSubsystemIterator::new(host);
+            for subsys in subsysiter {
+                let cstr = unsafe {
+                    std::ffi::CStr::from_ptr(crate::nvme_subsystem_get_nqn(
+                        subsys,
+                    ))
+                };
+                if cstr.to_str().unwrap() != self.subsysnqn {
+                    continue;
+                }
+                let ctrlriter = NvmeCtrlrIterator::new(subsys);
+                for c in ctrlriter {
+                    let ret = unsafe { crate::nvme_disconnect_ctrl(c) };
+                    if ret == 0 {
+                        i += 1;
+                    } else {
+                        return Err(NvmeError::FileIoError {
+                            rc: ret,
+                        });
+                    }
+                }
+            }
+        }
+        Ok(i)
+    }
+}
+
+#[test]
+fn nvme_parse_uri() {
+    let target =
+        NvmeTarget::try_from("nvmf://1.2.3.4:1234/testnqn.what-ever.foo")
+            .unwrap();
+
+    assert_eq!(target.trsvcid, 1234);
+    assert_eq!(target.traddr, "1.2.3.4");
+    assert_eq!(target.trtype, NvmeTransportType::Tcp);
+    assert_eq!(target.subsysnqn, "testnqn.what-ever.foo");
+
+    let target =
+        NvmeTarget::try_from("nvmf+tcp://1.2.3.4:1234/testnqn.what-ever.foo")
+            .unwrap();
+
+    assert_eq!(target.trsvcid, 1234);
+    assert_eq!(target.traddr, "1.2.3.4");
+    assert_eq!(target.trtype, NvmeTransportType::Tcp);
+    assert_eq!(target.subsysnqn, "testnqn.what-ever.foo");
+}
