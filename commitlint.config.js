37f211c06d8365dd98a07a5e45de198f15da5350
Tiago Castro <tiagolobocastro@gmail.com>
2024-01-25 16:45:37 +0000

    chore: stricter commitlint checks for pr merge
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,10 +2,25 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'example']],
+    'code-review-rule': [0, 'always'],
   },
   defaultIgnores: false,
   ignores: [
-    (message) => message.startsWith('chore(bors): merge pull request #'),
-    (message) => message.startsWith('Merge #')
-  ]
+      (message) => message.startsWith('chore(bors): merge pull request #'),
+      (message) => message.startsWith('Merge #')
+  ],
+  plugins: [
+    {
+      rules: {
+        'code-review-rule': ({subject}) => {
+          const REVIEW_COMMENTS = `Please don't merge code-review commits, instead squash them in the parent commit`;
+          if (subject.includes('code-review')) return [ false, REVIEW_COMMENTS ];
+          if (subject.includes('review comment')) return [ false, REVIEW_COMMENTS ];
+          if (subject.includes('address comment')) return [ false, REVIEW_COMMENTS ];
+          if (subject.includes('addressed comment')) return [ false, REVIEW_COMMENTS ];
+          return [ true ];
+        },
+      },
+    },
+  ],
 }
