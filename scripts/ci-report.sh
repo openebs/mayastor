103a29f2651950efc875d9ce4b24bdb926d89482
Prateek Chandra <Prateek.Chandra@datacore.com>
2025-05-12 15:34:16 +0530

    chore(ci): inject github token post nix installation
    Signed-off-by: Prateek Chandra <Prateek.Chandra@datacore.com>

--- scripts/ci-report.sh
+++ scripts/ci-report.sh
@@ -10,8 +10,7 @@ set -eu
 mkdir -p "$ROOT_DIR/ci-report"
 cd "$ROOT_DIR/ci-report"
 
-# Disable until we find how some CI envs are being included
-# journalctl --since="$CI_REPORT_START_DATE" -o short-precise > journalctl.txt
+journalctl --since="$CI_REPORT_START_DATE" -o short-precise | sed -E 's/ghs_[^[:space:]]+/ghs_***/g' > journalctl.txt
 journalctl -k -b0 -o short-precise > dmesg.txt
 lsblk -tfa > lsblk.txt
 $SUDO nvme list -v > nvme.txt
