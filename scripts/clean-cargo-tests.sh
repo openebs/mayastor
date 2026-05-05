1b35f06fb6d21d217eefe2a71c8903ac639fcd93
Tiago Castro <tiagolobocastro@gmail.com>
2026-05-05 14:12:16 +0100

    test: clean up stale lvm and loop devices properly
    The backing file for the loop may have been deleted, so rather than
    inspecting the back directory use losetup itself to find the leaked
    devices.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- scripts/clean-cargo-tests.sh
+++ scripts/clean-cargo-tests.sh
@@ -3,6 +3,11 @@
 SCRIPT_DIR="$(dirname "$0")"
 ROOT_DIR=$(realpath "$SCRIPT_DIR/..")
 
+if ! command -v nix-sudo >/dev/null; then
+  echo "Please run this from the nix-shell environment" >&2
+  exit 1
+fi
+
 nix-sudo nvme disconnect-all
 
 # Clean up ublk devices
@@ -15,19 +20,19 @@ nix-sudo ublk list -v | jq -r --arg dir "$back_dir" '
     nix-sudo ublk del -n "$id" --async
 done
 
-# Detach any loop devices created for test purposes
-for back_file in "/tmp/io-engine-tests"/*; do
-    # Find loop devices associated with the disk image
-    devices=$(losetup -j "$back_file" -O NAME --noheadings)
+for device in $(losetup -l -J | jq -r --arg tmp_dir $back_dir '.loopdevices[]|select(."back-file" | startswith($tmp_dir)) | .name'); do
+  echo "Found stale loop device: $device"
 
-    # Detach each loop device found
-    while IFS= read -r device; do
-        if [ -n "$device" ]; then
-            echo "Detaching loop device: $device"
-            sudo losetup -d "$device"
-        fi
-    done <<< "$devices"
+  nix-sudo $(which vgremove) -y --select="pv_name=$device" || :
+  nix-sudo $(which pvremove) -y "$device" || :
+  sudo losetup -d "$device" || :
+
+  for file in $(losetup -l -J | jq -r --arg tmp_dir $back_dir --arg dev $device '.loopdevices[]|select((."back-file" | startswith($tmp_dir)) and .name == $dev) | ."back-file"'); do
+    [ "$file" == "(deleted)" ] && continue;
+    echo "Left stale file: $file"
+  done
 done
+
 # Delete the directory too
 nix-sudo rmdir --ignore-fail-on-non-empty "/tmp/io-engine-tests" 2>/dev/null
 
