e988d32b6caa5c873eb9226d58eb7f2688eccf2a
Tiago Castro <tiagolobocastro@gmail.com>
2026-05-05 18:08:31 +0100

    fix(handle/guard): store current thread in the handle guard
    When a device handle is dropped, it gets sent to the primary thread.
    However, this doesn't work when we open the device handle in another thread,
    which is how I found this whilst testing writing directly from another
    reactors thread.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- io-engine/src/bdev/device.rs
+++ io-engine/src/bdev/device.rs
@@ -1,16 +1,10 @@
 //!
 //! Trait implementation for native bdev
 
-use std::{
-    collections::HashMap,
-    convert::TryFrom,
-    os::raw::c_void,
-    sync::{Arc, Mutex},
-};
-
 use async_trait::async_trait;
 use nix::errno::Errno;
 use once_cell::sync::{Lazy, OnceCell};
+use std::{collections::HashMap, convert::TryFrom, os::raw::c_void, rc::Rc, sync::Mutex};
 
 use spdk_rs::{
     libspdk::{
@@ -137,11 +131,11 @@ impl BlockDevice for SpdkBlockDevice {
 
 /// Wrapper around native SPDK block device descriptor, which mimics target SPDK
 /// descriptor as an abstract BlockDeviceDescriptor instance.
-struct SpdkBlockDeviceDescriptor(Arc<UntypedDescriptorGuard>);
+struct SpdkBlockDeviceDescriptor(Rc<UntypedDescriptorGuard>);
 
 impl From<UntypedDescriptorGuard> for SpdkBlockDeviceDescriptor {
     fn from(descr: UntypedDescriptorGuard) -> Self {
-        Self(Arc::new(descr))
+        Self(Rc::new(descr))
     }
 }
 
@@ -182,10 +176,10 @@ struct SpdkBlockDeviceHandle {
     handle: UntypedBdevHandle,
 }
 
-impl TryFrom<Arc<UntypedDescriptorGuard>> for SpdkBlockDeviceHandle {
+impl TryFrom<Rc<UntypedDescriptorGuard>> for SpdkBlockDeviceHandle {
     type Error = CoreError;
 
-    fn try_from(desc: Arc<UntypedDescriptorGuard>) -> Result<Self, Self::Error> {
+    fn try_from(desc: Rc<UntypedDescriptorGuard>) -> Result<Self, Self::Error> {
         let handle = BdevHandle::try_from(desc)?;
         Ok(SpdkBlockDeviceHandle::from(handle))
     }
