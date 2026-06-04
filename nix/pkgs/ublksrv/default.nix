a3d6ac3fd2d5364c43b591776a96ffafb080915d
Tiago Castro <tiagolobocastro@gmail.com>
2026-06-04 12:00:35 +0100

    build: ensure ublk build is deterministic
    We were leaving the .git which is not deterministic.
    Also set the current version.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- nix/pkgs/ublksrv/default.nix
+++ nix/pkgs/ublksrv/default.nix
@@ -1,19 +1,23 @@
 { stdenv, lib, fetchFromGitHub, pkgs }:
 
 stdenv.mkDerivation rec {
-  version = "v1.6";
+  version = "v1.7";
   pname = "ublksrv";
 
   src = fetchFromGitHub {
     owner = "ublk-org";
     repo = "ublksrv";
-    rev = version;
-    sha256 = "sha256-bK+cf/qdlbyQ2gSObk8cqDkIWjaneNIpnlZDyywezf8=";
-    leaveDotGit = true;
+    tag = version;
+    sha256 = "sha256-g/qRWe3BReJ9RbEw/cFqpgUSMPzn/haS4WY4Hz9t+fw=";
   };
 
-  nativeBuildInputs = with pkgs; [ pkg-config autoreconfHook git ];
+  nativeBuildInputs = with pkgs; [ pkg-config autoreconfHook ];
   buildInputs = with pkgs; [ liburing ];
+
+  # fixup the version as the maintainer forgot to update the VERSION file..
+  prePatch = ''
+    echo ${version} > VERSION
+  '';
 
   meta = {
     description = "The userspace part of the ublk framework";
@@ -23,4 +27,3 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/ublk-org/ublksrv";
   };
 }
-
